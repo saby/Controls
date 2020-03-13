0df827ac6e012bd29c0fb63eb75be8622f6d237f
oa.kashin <oa.kashin@tensor.ru>
2020-03-13 11:28:49 +0300

    Error Container cannot display dialog if there is no wrapping Controls/popup:Global. https://online.sbis.ru/opendoc.html?guid=3d0a8dcc-4820-4a8b-9a7a-984d2ec4311b

--- Controls/_dataSource/_error/Container.ts
+++ Controls/_dataSource/_error/Container.ts
@@ -1,6 +1,6 @@
 /// <amd-module name="Controls/_dataSource/_error/Container" />
-import Control = require('Core/Control');
-import template = require('wml!Controls/_dataSource/_error/Container');
+import {Control, TemplateFunction} from 'UI/Base';
+import _template = require('wml!Controls/_dataSource/_error/Container');
 // @ts-ignore
 import { constants } from 'Env/Env';
 import { ViewConfig } from 'Controls/_dataSource/_error/Handler';
@@ -10,6 +10,7 @@ import { isEqual } from 'Types/object';
 // @ts-ignore
 import { load } from 'Core/library';
 import { default as IContainer, IContainerConfig } from "Controls/_dataSource/_error/IContainer";
+import Popup from './Popup';
 
 /**
  * @interface Controls/dataSource/error/Container/Config
@@ -50,20 +51,19 @@ export default class Container extends Control implements IContainer {
     /**
      * @cfg {Controls/_dataSource/_error/Container/Config} Режим отображения
      */
-    private __viewConfig: Config;
-    private __lastShowedId: number;
-    protected _template = template;
-    _options: IContainerConfig;
-    _forceUpdate;
-    _notify;
+    private _viewConfig: Config;
+    private _lastShowedId: number;
+    private _popupHelper: Popup = new Popup();
+
+    protected _template: TemplateFunction = _template;
     /**
      * Скрыть компонент, отображающий данные об ошибке
      * @method
      * @public
      */
     hide(): void {
-        let mode = this.__viewConfig.mode;
-        this.__setConfig(null);
+        let mode = this._viewConfig.mode;
+        this._setConfig(null);
         if (mode == Mode.dialog) {
             return;
         }
@@ -77,71 +77,78 @@ export default class Container extends Control implements IContainer {
      */
     show(viewConfig: ViewConfig) {
         if (viewConfig.mode == Mode.dialog) {
-            return this.__showDialog(viewConfig)
+            return this._showDialog(viewConfig)
         }
-        this.__setConfig(viewConfig);
+        this._setConfig(viewConfig);
         this._forceUpdate();
     }
     protected _beforeMount(options: IContainerConfig) {
-        this.__updateConfig(options);
+        this._updateConfig(options);
     }
     protected _beforeUpdate(options: IContainerConfig) {
         if (isEqual(options.viewConfig, this._options.viewConfig)) {
             return;
         }
-        this.__updateConfig(options);
+        this._updateConfig(options);
     }
     protected _afterMount() {
-        if (this.__viewConfig) {
-            this.__showDialog(this.__viewConfig);
+        if (this._viewConfig) {
+            this._showDialog(this._viewConfig);
         }
     }
     protected _afterUpdate() {
-        if (this.__viewConfig) {
-            this.__showDialog(this.__viewConfig);
+        if (this._viewConfig) {
+            this._showDialog(this._viewConfig);
         }
     }
-    private __showDialog(config: Config) {
+    private _showDialog(config: Config): void {
         if (
             config.isShowed ||
-            config.mode != Mode.dialog ||
-            config.getVersion && config.getVersion() == this.__lastShowedId ||
+            config.mode !== Mode.dialog ||
+            config.getVersion && config.getVersion() === this._lastShowedId ||
             !constants.isBrowserPlatform
         ) {
             return;
         }
-        this.__lastShowedId = config.getVersion && config.getVersion();
+
+        this._lastShowedId = config.getVersion && config.getVersion();
         config.isShowed = true;
+
         getTemplate(config.template).then((template) => {
-            let result: Promise<void> = this._notify('serviceError', [
+            let result = this._notify('serviceError', [
                 template,
                 config.options,
                 this
             ], { bubbling: true });
+
+            if (!result) {
+                result = this._popupHelper.openDialog(config, this);
+            }
+
             if (result) {
-                result.then(this.__notifyDialogClosed.bind(this));
+                (result as Promise<null>).then(this._notifyDialogClosed.bind(this));
             }
         });
     }
-    private __notifyDialogClosed() {
+    private _notifyDialogClosed() {
         this._notify('dialogClosed', []);
     }
-    private __updateConfig(options: IContainerConfig) {
-        this.__setConfig(options.viewConfig);
-        if (this.__viewConfig) {
-            this.__viewConfig.isShowed = this.__viewConfig.isShowed || this.__viewConfig.mode !== Mode.dialog;
+    private _updateConfig(options: IContainerConfig) {
+        this._setConfig(options.viewConfig);
+        if (this._viewConfig) {
+            this._viewConfig.isShowed = this._viewConfig.isShowed || this._viewConfig.mode !== Mode.dialog;
         }
     }
-    private __setConfig(viewConfig?: ViewConfig) {
+    private _setConfig(viewConfig?: ViewConfig) {
         if (!viewConfig) {
-            this.__viewConfig = null;
+            this._viewConfig = null;
             return;
         }
         let templateName: string;
         if (typeof viewConfig.template == 'string') {
             templateName = viewConfig.template;
         }
-        this.__viewConfig = {
+        this._viewConfig = {
             ...viewConfig,
             templateName
         };
