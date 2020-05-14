3ad8fc52b465152e667a796a93d9a3d8960b4b76
ps.kolesova <ps.kolesova@tensor.ru>
2020-05-14 10:59:50 +0300

    https://online.sbis.ru/opendoc.html?guid=c4d4ed51-c68e-4f1c-865e-54efe324dc60

--- Controls/_dateRange/Link.ts
+++ Controls/_dateRange/Link.ts
@@ -1,8 +1,8 @@
 import BaseControl = require('Core/Control');
 import ILinkView from './interfaces/ILinkView';
 import componentTmpl = require('wml!Controls/_dateRange/Link/Link');
-import 'css!theme?Controls/dateRange';
 import getOptions from 'Controls/Utils/datePopupUtils';
+import LinkView from "./LinkView";
 /**
  * Controls that allows user to select date value in calendar.
  *
@@ -65,6 +65,8 @@ var Component = BaseControl.extend({
       this._notify('valueChanged', [value]);
    }
 });
+
+Component._theme = ['Controls/dateRange'];
 
 Component.EMPTY_CAPTIONS = ILinkView.EMPTY_CAPTIONS;
 
