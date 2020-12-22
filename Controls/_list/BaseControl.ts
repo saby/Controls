4d967f78dab07670e3e8c2968ce47500992d7eda
pa.averkiev <pa.averkiev@tensor.ru>
2020-12-22 10:04:13 +0300

    https://online.sbis.ru/opendoc.html?guid=0e8e4bf4-c5ef-4b07-a586-6f8280d9b9fc

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -3155,6 +3155,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
     _loadingIndicatorState: null,
     _loadingIndicatorTimer: null,
     _showLoadingIndicatorImage: null,
+    _isUpLoadingIndicatorPlaceholderAllowed: null,
 
     _pagingCfg: null,
     _pagingVisible: false,
@@ -4512,7 +4513,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
     },
     // padding-top нужен только когда сверху есть данные и скролл проинициализирован
     _getViewStyles(): string {
-        if (this._shouldShowLoadingIndicator('beforeEmptyTemplate')) {
+        if (this._isUpLoadingIndicatorPlaceholderAllowed && this._shouldShowLoadingIndicator('beforeEmptyTemplate')) {
             return 'padding-top: 56px';
         }
         return this._attachLoadTopTriggerToNull ? 'padding-top: 1px' : '';
@@ -5617,6 +5618,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
         // нельзя делать это в процессе обновления или загрузки
         if (!this._loadingState && !this._updateInProgress && !this._scrollController?.getScrollTop()) {
             _private.attachLoadTopTriggerToNullIfNeed(this, this._options);
+            this._isUpLoadingIndicatorPlaceholderAllowed = true;
         }
         if (this._hideTopTrigger && !this._needScrollToFirstItem) {
             this._hideTopTrigger = false;
