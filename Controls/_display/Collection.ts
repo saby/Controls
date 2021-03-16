dde917ee3cbd93dd2efea6fb7703e1e40292b7df
ka.panihin <ka.panihin@tensor.ru>
2021-03-16 12:11:09 +0300

    https://online.sbis.ru/opendoc.html?guid=c5c93ca7-2692-4522-80a0-0070b52a3ac7

--- Controls/_display/Collection.ts
+++ Controls/_display/Collection.ts
@@ -3704,7 +3704,7 @@ export default class Collection<S extends EntityModel = EntityModel, T extends C
         const sortMap = [];
         const groupMap = [];
 
-        strategy.splice(start, 0, items);
+        strategy.splice(start, 0, items, IObservable.ACTION_ADD);
         innerIndex = strategy.getDisplayIndex(start);
 
         items.forEach((item, index) => {
@@ -3735,7 +3735,7 @@ export default class Collection<S extends EntityModel = EntityModel, T extends C
 
         count = count === undefined ? strategy.count - start : count;
 
-        result = strategy.splice(start, count);
+        result = strategy.splice(start, count, [], IObservable.ACTION_REMOVE);
         innerIndex = result.start = strategy.getDisplayIndex(start);
 
         this._filterMap.splice(innerIndex, count);
@@ -3753,7 +3753,7 @@ export default class Collection<S extends EntityModel = EntityModel, T extends C
      */
     protected _replaceItems(start: number, newItems: S[]): ISplicedArray<T> {
         const strategy = this._getItemsStrategy();
-        const result = strategy.splice(start, newItems.length, newItems) as ISplicedArray<T>;
+        const result = strategy.splice(start, newItems.length, newItems, IObservable.ACTION_REPLACE) as ISplicedArray<T>;
         result.start = strategy.getDisplayIndex(start);
 
         return result;
@@ -3772,8 +3772,8 @@ export default class Collection<S extends EntityModel = EntityModel, T extends C
         const strategy = this._getItemsStrategy();
         let movedItems;
 
-        movedItems = strategy.splice(oldIndex, length);
-        strategy.splice(newIndex, 0, movedItems);
+        movedItems = strategy.splice(oldIndex, length, [], IObservable.ACTION_MOVE);
+        strategy.splice(newIndex, 0, movedItems, IObservable.ACTION_MOVE);
         movedItems.oldIndex = strategy.getDisplayIndex(oldIndex);
 
         return movedItems;
