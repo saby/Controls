f5b36044f278311da4fd514fc19a0936ca4192df
va.kolesov <va.kolesov@tensor.ru>
2021-03-18 17:26:04 +0300

    https://online.sbis.ru/opendoc.html?guid=7ac9767c-1a89-437b-b5b3-921427518537

--- Controls-demo/gridNew/Sorting/SortingSelector/ArrowTitle/Index.ts
+++ Controls-demo/gridNew/Sorting/SortingSelector/ArrowTitle/Index.ts
@@ -1,45 +1,47 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import * as Template from 'wml!Controls-demo/gridNew/Sorting/SortingSelector/IconStyle/Template';
+import * as Template from 'wml!Controls-demo/gridNew/Sorting/SortingSelector/ArrowTitle/Template';
+import {Memory} from 'Types/source';
+import {getCountriesStats} from '../../../DemoHelpers/DataCatalog';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _sortingParams: object[] = [];
     private _sorting: object[] = [];
+    protected _viewSource: Memory;
 
     protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: getCountriesStats().getData()
+        });
         this._sortingParams = [
             {
-                title: 'По порядку',
+                title: 'Без сортировки',
                 paramName: null,
-                icon: 'Controls/sortIcons:non_sort',
-                iconStyle: 'readonly'
+                icon: 'Controls/sortIcons:non_sort'
             },
             {
                 title: 'По времени',
                 paramName: 'time',
                 icon: 'Controls/sortIcons:time',
-                iconStyle: 'secondary'
+                titleAsc: 'Сначала старые',
+                titleDesc: 'Сначала новые'
             },
             {
                 title: 'По оценкам',
                 paramName: 'rating',
                 icon: 'Controls/sortIcons:rating',
-                iconStyle: 'label'
+                titleAsc: 'Сначала худшие',
+                titleDesc: 'Сначала лучшие'
             },
             {
                 title: 'По цене',
                 paramName: 'price',
                 icon: 'Controls/sortIcons:price',
-                iconStyle: 'danger'
-            },
-            {
-                title: 'По дате',
-                paramName: 'date',
-                icon: 'Controls/sortIcons:date',
-                iconStyle: 'success'
+                titleAsc: 'Сначала дешевые',
+                titleDesc: 'Сначала дорогие'
             }
         ];
-        this._sorting.push({time: 'ASC'});
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
---
+++ Controls-demo/gridNew/Sorting/SortingSelector/IconsSvg/Index.ts
@@ -0,0 +1,102 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/gridNew/Sorting/SortingSelector/IconsSvg/Template';
+import {Memory} from 'Types/source';
+import {getCountriesStats} from '../../../DemoHelpers/DataCatalog';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _sortingParams: object[] = [];
+    private _sorting: object[] = [];
+    protected _viewSource: Memory;
+
+    protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: getCountriesStats().getData()
+        });
+        this._sortingParams = [
+            {
+                title: 'Без сортировки',
+                paramName: null,
+                icon: 'Controls/sortIcons:non_sort'
+            },
+            {
+                title: 'По алфивиту (рус)',
+                paramName: 'alphabet_ru',
+                icon: 'Controls/sortIcons:alphabet_ru'
+            },
+            {
+                title: 'По алфивиту (eng)',
+                paramName: 'alphabet_en',
+                icon: 'Controls/sortIcons:alphabet_en'
+            },
+            {
+                title: 'По времени',
+                paramName: 'time',
+                icon: 'Controls/sortIcons:time'
+            },
+            {
+                title: 'По дате',
+                paramName: 'date',
+                icon: 'Controls/sortIcons:date'
+            },
+            {
+                title: 'По доглам',
+                paramName: 'debt',
+                icon: 'Controls/sortIcons:debt'
+            },
+            {
+                title: 'По отклонению',
+                paramName: 'deflection',
+                icon: 'Controls/sortIcons:deflection'
+            },
+            {
+                title: 'По количеству',
+                paramName: 'number',
+                icon: 'Controls/sortIcons:number'
+            },
+            {
+                title: 'По людям',
+                paramName: 'partner',
+                icon: 'Controls/sortIcons:partner'
+            },
+            {
+                title: 'По отзывам',
+                paramName: 'recall',
+                icon: 'Controls/sortIcons:recall'
+            },
+            {
+                title: 'По проблемам',
+                paramName: 'problem',
+                icon: 'Controls/sortIcons:problem'
+            },
+            {
+                title: 'По оценкам',
+                paramName: 'rating',
+                icon: 'Controls/sortIcons:rating'
+            },
+            {
+                title: 'По сроку',
+                paramName: 'timing',
+                icon: 'Controls/sortIcons:timing'
+            },
+            {
+                title: 'По сумме',
+                paramName: 'sum',
+                icon: 'Controls/sortIcons:sum'
+            },
+            {
+                title: 'По цене',
+                paramName: 'price',
+                icon: 'Controls/sortIcons:price'
+            },
+            {
+                title: 'По сложности',
+                paramName: 'difficult_sort',
+                icon: 'Controls/sortIcons:difficult_sort'
+            }
+        ];
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
