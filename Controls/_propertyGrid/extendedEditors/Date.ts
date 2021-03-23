c415aef782df93b539e6dbda68b81182912f9faf
is.zhurkov <is.zhurkov@tensor.ru>
2021-03-23 16:55:19 +0300

    https://online.sbis.ru/opendoc.html?guid=63df4160-6638-4dfe-9384-2be7c42cfc2a

---
+++ Controls/_propertyGrid/extendedEditors/Date.ts
@@ -0,0 +1,36 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import {SyntheticEvent} from 'Vdom/Vdom';
+import IEditor from 'Controls/_propertyGrid/IEditor';
+import IEditorOptions from 'Controls/_propertyGrid/IEditorOptions';
+import * as template from 'wml!Controls/_propertyGrid/extendedEditors/Date';
+
+/**
+ * Редактор для данных с типом "дата".
+ * @class Controls/_propertyGrid/extendedEditors/DateEditor
+ * @extends UI/Base:Control
+ * @author Журков И.С.
+ * @public
+ */
+class DateEditor extends Control<IEditorOptions> implements IEditor {
+    protected _template: TemplateFunction = template;
+    protected _value: unknown = null;
+
+    protected _beforeMount(options?: IEditorOptions): void {
+        this._updateValue(options.propertyValue);
+    }
+
+    protected _beforeUpdate(options?: IEditorOptions): void {
+        if (this._options.propertyValue !== options.propertyValue) {
+            this._updateValue(options.propertyValue);
+        }
+    }
+
+    protected _handleInputCompleted(event: SyntheticEvent, value: unknown): void {
+        this._notify('propertyValueChanged', [value], {bubbling: true});
+    }
+
+    private _updateValue(newValue: unknown): void {
+        this._value = newValue;
+    }
+}
+export default DateEditor;
--- Controls/propertyGrid.ts
+++ Controls/propertyGrid.ts
@@ -11,6 +11,7 @@
  * @includes BooleanGroupEditor Controls/_propertyGrid/extendedEditors/BooleanGroup
  * @includes FlatEnumEditor Controls/_propertyGrid/extendedEditors/FlatEnum
  * @includes TimeIntervalEditor Controls/_propertyGrid/extendedEditors/TimeInterval
+ * @includes DateEditor Controls/_propertyGrid/extendedEditors/Date
  * @includes CheckboxGroupEditor Controls/_propertyGrid/extendedEditors/CheckboxGroup
  * @includes DropdownEditor Controls/_propertyGrid/extendedEditors/Dropdown
  * @includes IPropertyGrid Controls/propertyGrid:GroupTemplate
@@ -32,6 +33,7 @@
  * @includes BooleanGroupEditor Controls/_propertyGrid/extendedEditors/BooleanGroup
  * @includes FlatEnumEditor Controls/_propertyGrid/extendedEditors/FlatEnum
  * @includes TimeIntervalEditor Controls/_propertyGrid/extendedEditors/TimeInterval
+ * @includes DateEditor Controls/_propertyGrid/extendedEditors/Date
  * @includes CheckboxGroupEditor Controls/_propertyGrid/extendedEditors/CheckboxGroup
  * @includes DropdownEditor Controls/_propertyGrid/extendedEditors/Dropdown
  * @includes IPropertyGrid Controls/propertyGrid:GroupTemplate
@@ -53,6 +55,7 @@ import FlatEnumEditor = require("Controls/_propertyGrid/extendedEditors/FlatEnum
 import {default as DropdownEditor} from 'Controls/_propertyGrid/extendedEditors/Dropdown';
 import {default as CheckboxGroupEditor} from 'Controls/_propertyGrid/extendedEditors/CheckboxGroup';
 import {default as TimeIntervalEditor} from 'Controls/_propertyGrid/extendedEditors/TimeInterval';
+import {default as DateEditor} from 'Controls/_propertyGrid/extendedEditors/Date';
 
 import IPropertyGrid = require("Controls/_propertyGrid/IPropertyGrid");
 import IEditor = require("Controls/_propertyGrid/IEditor");
@@ -70,6 +73,7 @@ export {
     NumberEditor,
     BooleanGroupEditor,
     FlatEnumEditor,
+    DateEditor,
     TimeIntervalEditor,
     CheckboxGroupEditor,
     DropdownEditor,
