50d30009eeb1184e7438d4eb5eb4bbd3079dd20b
se.mihaylov <se.mihaylov@tensor.ru>
2021-03-23 17:44:18 +0300

    https://online.sbis.ru/opendoc.html?guid=9e353530-aa2f-4997-8797-31247c25c26d

---
+++ Controls/_operationsPanel/_interface/IOperationsPanel.ts
@@ -0,0 +1,28 @@
+import {IControlOptions, TemplateFunction} from 'UI/Base';
+import {IToolBarItem} from 'Controls/toolbars';
+import {Memory} from 'Types/source';
+import {RecordSet} from 'Types/collection';
+import {TKey} from 'Controls/interface';
+
+export interface IActionOptions {
+    providerName: string;
+    providerOptions: Record<string, any>;
+    [key: string]: any;
+}
+
+export interface IOperationsPanelItem extends IToolBarItem {
+    actionName?: string;
+    actionOptions: IActionOptions;
+}
+
+export interface IOperationsPanelOptions extends IControlOptions {
+    source?: Memory;
+    items?: RecordSet | IOperationsPanelItem[];
+    keyProperty: string;
+    selectedKeys: TKey[];
+    excludedKeys: TKey[];
+    itemTemplate?: TemplateFunction;
+    operationsPanelOpenedCallback: () => void;
+    itemTemplateProperty?: string;
+    parentProperty?: string;
+}
