saby/Wasaby.Controls

tabs:Buttons: Сделано анимированное переключение https://online.sbis.ru/opendoc.html?guid=60b73c52-162b-423b-9f88-7cab356b318b

tests/ControlsUnit/Tabs/Buttons.test.js
@@ -138,30 +138,31 @@ define([
                inlineHeight: 's',
                selectedKey: '15',
                keyProperty: 'karambola',
-               theme: 'default'
+               theme: 'default',
+               horizontalPadding: 'm'
             },
             expected = 'controls-Tabs__item controls-Tabs__item_theme_default ' +
                'controls-Tabs__item_inlineHeight-s_theme-default ' +
                'controls-Tabs__item_align_left controls-Tabs__item_align_left_theme_default' +
                ' controls-Tabs__item_extreme controls-Tabs__item_extreme_theme_default ' +
-               'controls-Tabs__item_extreme_first controls-Tabs__item_extreme_first_theme_default ' +
+               'controls-Tabs__item_extreme_first controls-Tabs__item_extreme_first_horizontalPadding-m_theme_default ' +
                'controls-Tabs__item_notShrink',
             expected2 = 'controls-Tabs__item controls-Tabs__item_theme_default' +
                ' controls-Tabs__item_inlineHeight-s_theme-default' +
                ' controls-Tabs__item_align_right controls-Tabs__item_align_right_theme_default' +
-               ' controls-Tabs__item_default controls-Tabs__item_default_theme_default' +
+               ' controls-Tabs__item_default controls-Tabs__item_default_horizontalPadding-m_theme_default' +
                ' controls-Tabs__item_type_photo controls-Tabs__item_type_photo_theme_default ' +
                'controls-Tabs__item_notShrink',
             expected3 = 'controls-Tabs__item controls-Tabs__item_theme_default ' +
                'controls-Tabs__item_inlineHeight-s_theme-default ' +
                'controls-Tabs__item_align_right controls-Tabs__item_align_right_theme_default' +
-               ' controls-Tabs__item_default controls-Tabs__item_default_theme_default' +
+               ' controls-Tabs__item_default controls-Tabs__item_default_horizontalPadding-m_theme_default' +
                ' controls-Tabs__item_canShrink',
             expected4 = 'controls-Tabs__item controls-Tabs__item_theme_default ' +
                'controls-Tabs__item_inlineHeight-s_theme-default ' +
                'controls-Tabs__item_align_right controls-Tabs__item_align_right_theme_default' +
                ' controls-Tabs__item_extreme controls-Tabs__item_extreme_theme_default' +
-               ' controls-Tabs__item_extreme_last controls-Tabs__item_extreme_last_theme_default' +
+               ' controls-Tabs__item_extreme_last controls-Tabs__item_extreme_last_horizontalPadding-m_theme_default' +
                ' controls-Tabs__item_notShrink';
           const tabInstance = new tabsMod.Buttons();
           tabInstance.saveOptions(options);
@@ -192,8 +193,9 @@ define([
                theme: 'default'
             },
             expected = 'controls-Tabs_style_secondary__item_state_selected ' +
-            'controls-Tabs_style_secondary__item_state_selected_theme_default' +
-            ' controls-Tabs__item_state_selected controls-Tabs__item_state_selected_theme_default',
+               'controls-Tabs_style_secondary__item_state_selected_theme_default' +
+               ' controls-Tabs__item_state_selected controls-Tabs__item_state_selected_theme_default ' +
+               'controls-Tabs_style_secondary__item-marker_state_selected_theme_default',
             expected2 = 'controls-Tabs__item_state_default controls-Tabs__item_state_default_theme_default';
          const tabs = new tabsMod.Buttons();
          tabs.saveOptions(options);
@@ -285,6 +287,35 @@ define([
          tabs._onItemClick(event1, 1);
          assert.equal(notifyCorrectCalled, true, 'leftButtonClick _onItemClick');
          tabs.destroy();
+      });
+
+      describe('_updateMarker', () => {
+         it('should update marker model', () => {
+            const tabs = new tabsMod.Buttons();
+            let items = new collection.RecordSet({
+               rawData: data,
+               keyProperty: 'id'
+            });
+
+            const getBoundingClientRect = () => {
+               return { width: 10, left: 20 };
+            };
+
+            tabs._container = { getBoundingClientRect };
+
+            tabs._children = {};
+            for (let i = 0; i < data.length; i++) {
+               tabs._children[`Tab${i}`] = { getBoundingClientRect };
+            }
+
+            tabs._beforeUpdate({ items, selectedKey: 1 });
+            tabs._updateMarker();
+            assert.equal(tabs._marker.getLeft(), 0, 'leftButtonClick _onItemClick');
+            assert.equal(tabs._marker.getWidth(), 10, 'leftButtonClick _onItemClick');
+
+            tabs.destroy();
+         });
+
       });
    });
 });

saby/Controls

tests/ControlsUnit/Tabs/Buttons.test.js
@@ -191,8 +191,9 @@ define([
                keyProperty: 'karambola',
                theme: 'default'
             },
-            expected = 'controls-Tabs_style_secondary__item_state_selected' +
-            ' controls-Tabs__item_state_selected',
+            expected = 'controls-Tabs_style_secondary__item_state_selected ' +
+               'controls-Tabs__item_state_selected ' +
+               ' controls-Tabs_style_secondary__item-marker_state_selected',
             expected2 = 'controls-Tabs__item_state_default';
          const tabs = new tabsMod.Buttons();
          tabs.saveOptions(options);
@@ -284,6 +285,35 @@ define([
          tabs._onItemClick(event1, 1);
          assert.equal(notifyCorrectCalled, true, 'leftButtonClick _onItemClick');
          tabs.destroy();
+      });
+
+      describe('_updateMarker', () => {
+         it('should update marker model', () => {
+            const tabs = new tabsMod.Buttons();
+            let items = new collection.RecordSet({
+               rawData: data,
+               keyProperty: 'id'
+            });
+
+            const getBoundingClientRect = () => {
+               return { width: 10, left: 20 };
+            };
+
+            tabs._container = { getBoundingClientRect };
+
+            tabs._children = {};
+            for (let i = 0; i < data.length; i++) {
+               tabs._children[`Tab${i}`] = { getBoundingClientRect };
+            }
+
+            tabs._beforeUpdate({ items, selectedKey: 1 });
+            tabs._updateMarker();
+            assert.equal(tabs._marker.getLeft(), 0, 'leftButtonClick _onItemClick');
+            assert.equal(tabs._marker.getWidth(), 10, 'leftButtonClick _onItemClick');
+
+            tabs.destroy();
+         });
+
       });
    });
 });