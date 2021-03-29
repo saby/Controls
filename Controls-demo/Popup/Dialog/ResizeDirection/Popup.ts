eaf6d8fb999513b325cc8d1c461995438f21acec
in.kondratev <in.kondratev@tensor.ru>
2021-03-29 09:19:26 +0300

    fixes demostand on popup-dialog

--- Controls-demo/Popup/Dialog/ResizeDirection/Popup.ts
+++ Controls-demo/Popup/Dialog/ResizeDirection/Popup.ts
@@ -3,8 +3,8 @@ import controlTemplate = require('wml!Controls-demo/Popup/Dialog/ResizeDirection
 
 class Popup extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
-    protected _height: string = '150';
-    protected _width: string = '300';
+    protected _height: number = 150;
+    protected _width: number = 300;
     static _theme: string[] = ['Controls/Classes'];
     static _styles: string[] = ['Controls-demo/Popup/Dialog/ResizeDirection/Popup', 'Controls-demo/Controls-demo'];
     protected _sizeChangedHandler(): void {
