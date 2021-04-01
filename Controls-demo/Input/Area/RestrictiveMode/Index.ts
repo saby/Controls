7d58352517a0f4fd89c2a2f7d8a27dfb1bc93068
gd.kovalev <gd.kovalev@tensor.ru>
2021-04-01 18:14:59 +0700

    https://online.sbis.ru/opendoc.html?guid=1905ed30-ebb8-49c1-a0a3-ec934a3aa297

---
+++ Controls-demo/Input/Area/RestrictiveMode/Index.ts
@@ -0,0 +1,15 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import * as template from 'wml!Controls-demo/Input/Area/RestrictiveMode/RestrictiveMode';
+
+class TextAlignments extends Control<IControlOptions> {
+    protected _template: TemplateFunction = template;
+    protected _readOnly: boolean;
+
+    protected _toggleReadOnly(): void {
+        this._readOnly = !this._readOnly;
+    }
+    static _theme: string[] = ['Controls/Classes'];
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
+
+export default TextAlignments;
