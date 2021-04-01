aaa1b790a1bb4cee44634fe847068f01c6d3a596
gd.kovalev <gd.kovalev@tensor.ru>
2021-04-01 18:19:02 +0700

    https://online.sbis.ru/opendoc.html?guid=1905ed30-ebb8-49c1-a0a3-ec934a3aa297

--- Controls/spoiler.ts
+++ Controls/spoiler.ts
@@ -8,3 +8,4 @@
 export {default as Cut, ICutOptions} from 'Controls/_spoiler/Cut';
 export {default as View, IView, IViewOptions} from 'Controls/_spoiler/View';
 export {default as Heading, IHeading, IHeadingOptions} from 'Controls/_spoiler/Heading';
+export {default as AreaCut} from 'Controls/_spoiler/AreaCut';
