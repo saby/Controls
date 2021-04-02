036e981ff6246520f81ad320274556dc8e8148ec
in.kondratev <in.kondratev@tensor.ru>
2021-04-02 13:27:24 +0300

    https://online.sbis.ru/opendoc.html?guid=60aee4e5-8229-4c1d-8eae-7f6912849470

--- Controls-demo/Popup/Dialog/ResizeDirection/Popup.ts
+++ Controls-demo/Popup/Dialog/ResizeDirection/Popup.ts
@@ -3,20 +3,29 @@ import controlTemplate = require('wml!Controls-demo/Popup/Dialog/ResizeDirection
 
 class Popup extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
-    protected _height: number = 400;
-    protected _width: number = 650;
-    protected _applyHeight: number = 400;
-    protected _applyWidth: number = 650;
-    protected _minHeight: number = 400;
-    protected _minWidth: number = 650;
+    protected _height: number = 200;
+    protected _width: number = 300;
+    protected _minWidth: number = 300;
+    protected _minHeight: number = 200;
+    protected _resizeHeight: number = 400;
+    protected _resizeWidth: number = 600;
+    protected _enlargeButton: string = "Увеличить";
+    protected _reduceButton: string = "Уменьшить";
+    protected _buttonCaption: string = "Увеличить";
+
     static _theme: string[] = ['Controls/Classes'];
     static _styles: string[] = ['Controls-demo/Popup/Dialog/ResizeDirection/Popup', 'Controls-demo/Controls-demo'];
-    protected _sizeChangedHandler(): void {
-        this._notify('controlResize', [], {bubbling: true});
-    }
-    protected _handleClick(): void {
-        this._applyHeight = this._height < this._minHeight ? this._minHeight: this._height;
-        this._applyWidth = this._width < this._minWidth ? this._minWidth : this._width;
+
+    protected _handleResizeClick(): void {
+        if (this._height === 200 && this._width === 300) {
+            this._height = this._resizeHeight;
+            this._width = this._resizeWidth;
+            this._buttonCaption = this._reduceButton;
+        } else {
+            this._height = this._minHeight;
+            this._width = this._minWidth;
+            this._buttonCaption = this._enlargeButton;
+        }
     }
 }
 export default Popup;
