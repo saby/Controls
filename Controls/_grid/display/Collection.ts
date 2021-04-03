a3b9ee830a7c1927a41d311ba5a88f88dfc081b2
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-04-03 16:21:47 +0300

    Рефакторинг, без задачи

--- Controls/_grid/display/Collection.ts
+++ Controls/_grid/display/Collection.ts
@@ -38,7 +38,7 @@ export default class Collection<
         if (superResult) {
             if (this._$emptyTemplate) {
                 if (this._$emptyGridRow) {
-                    this._$emptyGridRow.setEmptyTemplate(this._$emptyTemplate);
+                    this._$emptyGridRow.setRowTemplate(this._$emptyTemplate);
                 } else {
                     this._initializeEmptyRow();
                 }
@@ -50,10 +50,13 @@ export default class Collection<
     }
 
     setEmptyTemplateOptions(options: object): boolean {
-        super.setEmptyTemplateOptions(options);
-        if (this.getEmptyGridRow()) {
-            this.getEmptyGridRow().setEmptyTemplateOptions(options);
+        if (super.setEmptyTemplateOptions(options)) {
+            if (this.getEmptyGridRow()) {
+                this.getEmptyGridRow().setRowTemplateOptions(options);
+            }
+            return true;
         }
+        return false;
     }
 
     setMultiSelectVisibility(visibility: string): void {
