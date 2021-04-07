a0fc54174977869b7790812fe4aee74d489f7331
in.kondratev <in.kondratev@tensor.ru>
2021-04-07 15:48:03 +0300

    https://online.sbis.ru/opendoc.html?guid=171ba585-78c9-4a15-ba51-e774010c5689

--- Controls-demo/toggle/RadioGroup/CustomItemTemplate/Index.ts
+++ Controls-demo/toggle/RadioGroup/CustomItemTemplate/Index.ts
@@ -1,11 +1,9 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import {Memory} from "Types/source";
 import Template = require('wml!Controls-demo/toggle/RadioGroup/CustomItemTemplate/Template');
-import itemTemplate = require('wml!Controls-demo/toggle/RadioGroup/CustomItemTemplate/resources/SingleItemTemplate');
 
 class CustomItemTemplate extends Control<IControlOptions> {
     protected _template: TemplateFunction = Template;
-    protected _itemTemplate: TemplateFunction = itemTemplate;
     protected _selectedKey: string = '1';
     protected _selectedKey2: string = '1';
     protected _source: Memory;
@@ -15,7 +13,6 @@ class CustomItemTemplate extends Control<IControlOptions> {
     protected _beforeMount(): void {
         this._source = new Memory({
             keyProperty: 'id',
-            displayProperty: 'caption',
             data: [{
                 id: '1',
                 title: 'State1',
@@ -24,7 +21,6 @@ class CustomItemTemplate extends Control<IControlOptions> {
                 id: '2',
                 title: 'State2',
                 caption: 'Additional caption2',
-                templateTwo: 'wml!Controls-demo/toggle/RadioGroup/CustomItemTemplate/resources/SingleItemTemplate'
             }, {
                 id: '3',
                 title: 'State3',
