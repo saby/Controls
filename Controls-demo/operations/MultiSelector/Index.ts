a3a1c735ea6a1032aef65d164ebc7176bfa2dbd9
mk.siryakov <mk.siryakov@tensor.ru>
2021-04-08 17:03:08 +0300

    https://online.sbis.ru/opendoc.html?guid=13dfc8eb-b425-4346-bc15-4aca7416f352

--- Controls-demo/operations/MultiSelector/Index.ts
+++ Controls-demo/operations/MultiSelector/Index.ts
@@ -22,9 +22,5 @@ export default class MultiSelectorCheckboxDemo extends Control {
         });
     }
 
-    _limitChangedHandler(limit): void {
-        alert(limit);
-    }
-
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
