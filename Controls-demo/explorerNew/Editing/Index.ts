f7f6bebcc16a53f14538f87eb672e1bd8ac2345b
ka.panihin <ka.panihin@tensor.ru>
2021-04-08 11:52:28 +0300

    https://online.sbis.ru/opendoc.html?guid=49d97d0f-87c4-4781-a8a2-329afe72cfe4

---
+++ Controls-demo/explorerNew/Editing/Index.ts
@@ -0,0 +1,20 @@
+import {Control, TemplateFunction} from "UI/Base"
+import * as Template from "wml!Controls-demo/Explorer/Editing/Editing"
+import Memory = require('Controls-demo/Explorer/ExplorerMemory')
+import {Gadgets} from '../../Explorer_new/DataHelpers/DataCatalog';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: Memory;
+    protected _columns = Gadgets.getGridEditingCol();
+
+
+    _beforeMount() {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: Gadgets.getData(),
+        });
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
---
+++ Controls-demo/explorerNew/Multiselect/Index.ts
@@ -0,0 +1,43 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/explorerNew/Multiselect/Multiselect';
+import {Gadgets} from '../DataHelpers/DataCatalog';
+import * as MemorySource from 'Controls-demo/Explorer/ExplorerMemory';
+import {IColumn} from 'Controls/grid';
+import {TRoot, IHeader} from 'Controls-demo/types';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: MemorySource;
+    protected _radioSource: MemorySource;
+    protected _columns: IColumn[] = Gadgets.getGridColumns();
+    protected _viewMode: string = 'table';
+    protected _root: TRoot = null;
+    protected _header: IHeader[] = Gadgets.getHeader();
+    protected _selectedKey: string = 'visible';
+
+    protected _beforeMount(): void {
+        this._viewSource = new MemorySource({
+            keyProperty: 'id',
+            data: Gadgets.getData()
+        });
+        this._radioSource = new MemorySource({
+            keyProperty: 'id',
+            data: [
+                {
+                    id: 'visible',
+                    title: 'multiSelectVisibility = visible',
+                },
+                {
+                    id: 'hidden',
+                    title: 'multiSelectVisibility = hidden',
+                },
+                {
+                    id: 'onhover',
+                    title: 'multiSelectVisibility = onhover',
+                }
+            ]
+        });
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
