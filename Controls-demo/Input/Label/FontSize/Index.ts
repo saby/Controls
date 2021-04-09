0e68c989fcff4ca07fedd1ed160d9e76ac126466
ma.mochalov <ma.mochalov@tensor.ru>
2021-04-09 10:49:45 +0300

    https://online.sbis.ru/opendoc.html?guid=fddd319f-06b7-4982-b30b-4c20f58189df

--- Controls-demo/Input/Label/FontSize/Index.ts
+++ Controls-demo/Input/Label/FontSize/Index.ts
@@ -3,8 +3,12 @@ import controlTemplate = require('wml!Controls-demo/Input/Label/FontSize/Index')
 
 class Href extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
+    protected _fontSizes: string[] = [
+        'inherit', 'xs', 's', 'm', 'l', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
+    ];
 
     static _theme: string[] = ['Controls/Classes'];
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
+
 export default Href;
