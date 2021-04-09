fcac1d4172a4e3f933e8fbbeb56b50b6daa1de05
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-04-09 12:15:13 +0300

    https://online.sbis.ru/opendoc.html?guid=28227752-8e89-45d2-9b20-853670907733

--- Controls/_treeGridOld/TreeGridView/TreeGridView.ts
+++ Controls/_treeGridOld/TreeGridView/TreeGridView.ts
@@ -3,12 +3,15 @@ import {GridView, GridLayoutUtil} from 'Controls/gridOld';
 
 import * as GridItemTemplate from 'wml!Controls/_treeGridOld/TreeGridView/layout/grid/Item';
 import * as TableItemTemplate from 'wml!Controls/_treeGridOld/TreeGridView/layout/table/Item';
+import * as nodeFooterTemplate from 'wml!Controls/_treeGridOld/TreeGridView/layout/common/NodeFooterChooser';
 import 'css!Controls/grid';
 import 'css!Controls/treeGrid';
 
 var
     TreeGridView = GridView.extend({
         _defaultItemTemplate: GridItemTemplate,
+        _nodeFooterTemplate: nodeFooterTemplate,
+
         _beforeUpdate(newCfg) {
             TreeGridView.superclass._beforeUpdate.apply(this, arguments);
             if (this._options.expanderSize !== newCfg.expanderSize) {
