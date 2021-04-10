9361b1d518feab153efcbb3b4adf9a77c9f3a47a
ma.mochalov <ma.mochalov@tensor.ru>
2021-04-10 13:53:24 +0300

    https://online.sbis.ru/opendoc.html?guid=56db386e-b359-4809-9eff-f5b38c8e547f

---
+++ Controls-demo/Popup/PreviewerTarget/ReadOnly/Index.ts
@@ -0,0 +1,8 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/Popup/PreviewerTarget/ReadOnly/ReadOnly';
+
+class Base extends Control<IControlOptions> {
+    protected _template: TemplateFunction = Template;
+}
+
+export default Base;
