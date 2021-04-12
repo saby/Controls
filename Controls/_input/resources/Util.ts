5d5a15be661a1171ced5840f260e3f6670a5503e
mk.siryakov <mk.siryakov@tensor.ru>
2021-04-12 17:16:48 +0300

    https://online.sbis.ru/opendoc.html?guid=a49423dd-b498-462f-b018-4bd869534f9f

--- Controls/_input/resources/Util.ts
+++ Controls/_input/resources/Util.ts
@@ -2,7 +2,6 @@ import {detection} from 'Env/Env';
 import {IText} from 'Controls/decorator';
 import {ISelection, ISplitValue, InputType, NativeInputType} from './Types';
 import {SyntheticEvent} from 'UI/Vdom';
-import {changeLayout} from 'I18n/keyboard';
 import {controller as i18Controller} from 'I18n/i18n';
 
 export const MINUS: string = '-';
@@ -107,6 +106,7 @@ export function transliterateInput(value: string, selection: ISelection): Promis
     ) || value;
     const firstLocale = i18Controller.currentLocale.indexOf('ru') !== -1 ? 'ru' : 'en';
     const secondLocale = firstLocale === 'ru' ? 'en' : 'ru';
+    const {changeLayout} = import('I18n/keyboard');
     return changeLayout(text, firstLocale, secondLocale).then((revertedText) => {
         if (revertedText === text) {
             return changeLayout(text, secondLocale, firstLocale).then((revertedText) => {
