c05010981c3d6edb897c88fa87bda155bd52a3b4
ka.panihin <ka.panihin@tensor.ru>
2021-04-13 12:56:37 +0300

    https://online.sbis.ru/opendoc.html?guid=357ab618-a819-440e-90f3-9d512a4975eb

--- tests/ControlsUnit/Tree/TreeControl/TreeControl.test.js
+++ tests/ControlsUnit/Tree/TreeControl/TreeControl.test.js
@@ -1911,8 +1911,8 @@ define([
          });
 
          it('call when model is not created', () => {
-            const treeControl = new tree.TreeControl({});
-            treeControl._beforeMount({});
+            const treeControl = new tree.TreeControl({keyProperty: 'id'});
+            treeControl._beforeMount({keyProperty: 'id'});
             assert.doesNotThrow(treeControl.resetExpandedItems.bind(treeControl));
          });
       });
