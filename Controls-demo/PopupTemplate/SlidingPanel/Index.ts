33e6a39e9dce751e7cb1882c4f967c34fb0bdbf6
dv.onischuk <dv.onischuk@tensor.ru>
2021-04-15 09:54:18 +0300

    https://online.sbis.ru/opendoc.html?guid=e4ecd6f5-f74d-452b-a295-5ee108ea574f

--- Controls-demo/PopupTemplate/SlidingPanel/Index.ts
+++ Controls-demo/PopupTemplate/SlidingPanel/Index.ts
@@ -1,8 +1,18 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import controlTemplate = require('wml!Controls-demo/PopupTemplate/SlidingPanel/Index/Index');
+import {detection} from 'Env/Env';
+
 
 class Index extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
+    protected _beforeMount(): void {
+        // В этой демке показывается поведение контрола в режиме мобилки
+        detection.isPhone = true;
+    }
+    protected _beforeUnmount(): void {
+        // На случай если в демках появится SPA, чтобы не поломать другие демки
+        detection.isPhone = false;
+    }
     static _theme: string[] = ['Controls/Classes'];
     static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/PopupTemplate/SlidingPanel/Index/Index'];
 }
