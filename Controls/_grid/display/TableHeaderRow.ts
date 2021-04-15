8c700cc650e0fce5146c0228a1a59cfe5d9021ba
va.kolesov <va.kolesov@tensor.ru>
2021-04-15 10:03:39 +0300

    https://online.sbis.ru/opendoc.html?guid=c1199b80-4e4a-4775-a4d0-12b6bd05c687

--- Controls/_grid/display/TableHeaderRow.ts
+++ Controls/_grid/display/TableHeaderRow.ts
@@ -4,10 +4,12 @@ export default class TableHeaderRow<T> extends HeaderRow<T> {
     getItemClasses(): string {
         return '';
     }
-
+    hasMultiSelectColumn(): boolean {
+        return this._$owner.hasMultiSelectColumn() && this._$headerModel.getRowIndex(this) === 0;
+    }
     protected _addCheckBoxColumnIfNeed(): void {
         const factory = this.getColumnsFactory();
-        if (this._$owner.hasMultiSelectColumn() && this._$headerModel.getRowIndex(this) === 0) {
+        if (this.hasMultiSelectColumn()) {
             const {start, end} = this._$headerModel.getBounds().row;
             this._$columnItems.unshift(factory({
                 column: {
