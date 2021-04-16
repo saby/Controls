e6d07edfefe58bbb2c7c960c04ed1b5943a20c1a
ka.panihin <ka.panihin@tensor.ru>
2021-04-16 16:38:32 +0300

    https://online.sbis.ru/opendoc.html?guid=b45d1d7f-a0ae-43c6-a02f-34a18a7a48e0

--- Controls/_explorer/View.ts
+++ Controls/_explorer/View.ts
@@ -375,10 +375,13 @@ export default class Explorer extends Control<IExplorerOptions> {
 
     protected _onItemClick(
         event: SyntheticEvent,
-        item: Model,
+        item: Model|Model[],
         clickEvent: SyntheticEvent,
         columnIndex?: number
     ): boolean {
+        if (item instanceof Array) {
+            item = item[item.length - 1];
+        }
 
         const res = this._notify('itemClick', [item, clickEvent, columnIndex]) as boolean;
         event.stopPropagation();
