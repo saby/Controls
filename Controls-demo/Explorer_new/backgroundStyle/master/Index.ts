1998236e603aa73c6522b4e199da79c0d1463464
ea.melnikova <ea.melnikova@tensor.ru>
2021-04-17 18:07:29 +0300

    https://online.sbis.ru/opendoc.html?guid=3c82d5ed-4ae2-4c08-a68c-12fec9791c4a

--- Controls-demo/Explorer_new/backgroundStyle/master/Index.ts
+++ Controls-demo/Explorer_new/backgroundStyle/master/Index.ts
@@ -60,7 +60,6 @@ export default class extends Control {
    }
 
    static _styles: string[] = [
-      'Controls-demo/Controls-demo',
-      'Controls-demo/Explorer_new/backgroundStyle/master/Index'
+      'Controls-demo/Controls-demo'
    ];
 }
