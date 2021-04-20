231f499b64ad43779b7e50d01f3f0ae9bbdd9dcd
as.zaycev <as.zaycev@tensor.ru>
2021-04-20 09:23:40 +0300

    https://online.sbis.ru/opendoc.html?guid=acf263ce-ff3d-4bb8-a0a3-1f4733fa0692

--- Controls/context.ts
+++ Controls/context.ts
@@ -2,11 +2,9 @@
  * Набор полей контекста, используемых в платформенных контролах.
  * @library Controls/context
  * @includes ContextOptions Controls/_context/ContextOptions
- * @includes TouchContextField Controls/_context/TouchContextField
  * @deprecated
  * @private
  * @author Крайнов Д.О.
  */
 
 export {default as ContextOptions} from 'Controls/_context/ContextOptions';
-export {default as TouchContextField} from 'Controls/_context/TouchContextField';
