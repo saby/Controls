33b0408c6a24ae4fa3ca302ae27c9055df3162e1
gd.kovalev <gd.kovalev@tensor.ru>
2021-04-23 17:08:18 +0700

    https://online.sbis.ru/opendoc.html?guid=30007512-9268-4641-bacc-baa43340a0fa

--- Controls/_shortDatePicker/bodyItem.ts
+++ Controls/_shortDatePicker/bodyItem.ts
@@ -44,9 +44,7 @@ class BodyItem extends Control<IShortDatePickerOptions> implements IDateConstruc
 
     protected _beforeUpdate(newOptions: IShortDatePickerOptions): void {
         this._yearModel = this._getYearModel(newOptions.currentYear, newOptions.dateConstructor);
-        if (newOptions.monthTemplate !== this._options.monthTemplate) {
-            this._isMonthsTemplateString = typeof newOptions.monthTemplate === 'string';
-        }
+        this._isMonthsTemplateString = typeof newOptions.monthTemplate === 'string';
     }
 
     protected _getItemTmplByType(options: IShortDatePickerOptions): TemplateFunction {
