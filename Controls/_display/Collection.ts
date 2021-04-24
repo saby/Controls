e97762d39e0a0101a8d4d8655c245985acaa5962
ka.panihin <ka.panihin@tensor.ru>
2021-04-24 12:33:16 +0300

    https://online.sbis.ru/opendoc.html?guid=0fb352c5-b87f-4780-855a-4fb8fb4c5540

--- Controls/_display/Collection.ts
+++ Controls/_display/Collection.ts
@@ -2448,7 +2448,11 @@ export default class Collection<S extends EntityModel = EntityModel, T extends C
         return this._$searchValue || '';
     }
 
-    getItemBySourceKey(key: string|number): T {
+    /**
+     * @param key
+     * @param [withFilter=true] Учитывать {@link setFilter фильтр}
+     */
+    getItemBySourceKey(key: string|number, withFilter: boolean = true): T {
         if (this._$collection['[Types/_collection/RecordSet]']) {
             if (key !== undefined) {
                 const record = (this._$collection as unknown as RecordSet).getRecordById(key);
@@ -2462,9 +2466,11 @@ export default class Collection<S extends EntityModel = EntityModel, T extends C
 
                     // Или требуется найти группу
                     return this.find((item) => item['[Controls/_display/GroupItem]'] && item.key === key);
-                }
-                else {
+                } else if (withFilter) {
                     return this.getItemBySourceItem(record as unknown as S);
+                } else {
+                    const items = this._getItems();
+                    return items.find((it) => it.getContents() === record);
                 }
             } else {
                 return null;
