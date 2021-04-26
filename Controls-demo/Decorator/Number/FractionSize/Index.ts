a4d5aed0360d01eeb261260417173afd0e7c0427
mk.siryakov <mk.siryakov@tensor.ru>
2021-04-26 10:11:37 +0300

    https://online.sbis.ru/opendoc.html?guid=83cb8769-4951-4e86-ae3d-bbad3a786c10

--- Controls-demo/Decorator/Number/FractionSize/Index.ts
+++ Controls-demo/Decorator/Number/FractionSize/Index.ts
@@ -1,14 +1,11 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import controlTemplate = require('wml!Controls-demo/Decorator/Number/FractionSize/FractionSize');
+import 'css!Controls/Classes';
+import 'css!Controls-demo/Controls-demo';
 
 class FractionSize extends Control<IControlOptions> {
     protected _value = '12345.67890';
-
     protected _template: TemplateFunction = controlTemplate;
-
-    static _theme: string[] = ['Controls/Classes'];
-
-    static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
 
 export default FractionSize;
