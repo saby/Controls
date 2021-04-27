8e1660f64c51c838b58b633388030348e2aa050e
ma.mochalov <ma.mochalov@tensor.ru>
2021-04-27 16:09:25 +0300

    https://online.sbis.ru/opendoc.html?guid=477a7da7-72f5-4af4-864c-46a77b14f336

--- Controls/_tabs/Buttons.ts
+++ Controls/_tabs/Buttons.ts
@@ -329,6 +329,9 @@ class TabsButtons extends Control<ITabsOptions> implements ITabsButtons, IItems,
         const style = TabsButtons._prepareStyle(options.style);
         if (item.isMainTab) {
             classes.push('controls-Tabs__item_state_main');
+            if (item[options.keyProperty] === options.selectedKey) {
+                classes.push('controls-Tabs__item_state_selected ');
+            }
         } else if (item[options.keyProperty] === options.selectedKey) {
             classes.push(`controls-Tabs_style_${style}__item_state_selected`);
             classes.push('controls-Tabs__item_state_selected ');
