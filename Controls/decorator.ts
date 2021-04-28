bdcc095c067df0df6a87229a93caf49371da5165
eu.koroleva <eu.koroleva@tensor.ru>
2021-04-28 15:09:37 +0300

    https://online.sbis.ru/opendoc.html?guid=cafb5da7-3660-43dd-88a9-171d82cf5084

--- Controls/decorator.ts
+++ Controls/decorator.ts
@@ -51,7 +51,7 @@ import {default as splitIntoTriads, concatTriads, NUMBER_DIGITS_TRIAD, SPLITTER}
 import {default as toString} from './_decorator/inputUtils/toString';
 import {default as numberToString} from './_decorator/inputUtils/numberToString';
 import {partOfNumber, escapeSpecialChars, addWordCheck} from './_decorator/inputUtils/RegExp';
-import {getLinks} from './_decorator/Markup/resources/linkDecorateUtils';
+import {getLinks, normalizeLink} from './_decorator/Markup/resources/linkDecorateUtils';
 
 export {
     Converter,
@@ -69,5 +69,6 @@ export {
     partOfNumber,
     escapeSpecialChars,
     addWordCheck,
-    getLinks
+    getLinks,
+    normalizeLink
 };
