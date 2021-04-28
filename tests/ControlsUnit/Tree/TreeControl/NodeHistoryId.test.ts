742030ea4164caa4c5ed93f7c9c31a0e0b486195
paaverkiev <pa.averkiev@corp.tensor.ru>
2021-04-28 23:11:36 +0300

    https://online.sbis.ru/opendoc.html?guid=fa070345-7f8d-4f2c-8f01-1890d2ca46f3

--- tests/ControlsUnit/Tree/TreeControl/NodeHistoryId.test.ts
+++ tests/ControlsUnit/Tree/TreeControl/NodeHistoryId.test.ts
@@ -88,33 +88,4 @@ describe('Controls/Tree/TreeControl/NodeHistoryId', () => {
             rawData: data
         });
     });
-
-    // На списке есть NodeHistoryId. Раскрываем узел. Должен произойти запрос на сохранение настроек.
-    it('should save UserConfig when nodeHistoryId is set', () => {
-        source = new HierarchicalMemory();
-        const spyStore = spy(nodeHistoryUtil, 'store');
-
-        const treeControl = initTreeControl({nodeHistoryId: 'HISTORY_ID'});
-        treeControl.toggleExpanded(1);
-        sinonAssert.calledWith(spyStore, ['1'], 'HISTORY_ID');
-        spyStore.restore();
-    });
-
-    // На списке есть NodeHistoryId. Поменяли NodeHistoryId. Раскрываем узел.
-    // Должен произойти запрос на сохранение настроек с новым NodeHistoryId.
-    it('should save UserConfig with updated nodeHistoryId', () => {
-        source = new HierarchicalMemory({
-            keyProperty: 'id',
-            data
-        });
-        const spyStore = spy(nodeHistoryUtil, 'store');
-
-        const treeControl = initTreeControl({nodeHistoryId: 'HISTORY_ID'});
-
-        treeControl.saveOptions({...config, nodeHistoryId: 'NEW_HISTORY_ID'});
-        treeControl._beforeUpdate({...config, nodeHistoryId: 'NEW_HISTORY_ID'});
-        treeControl.toggleExpanded(1);
-        sinonAssert.calledWith(spyStore, ['1'], 'NEW_HISTORY_ID');
-        spyStore.restore();
-    });
 });
