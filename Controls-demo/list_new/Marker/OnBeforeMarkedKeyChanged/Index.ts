35de6b0e1b35c2689272f23d2bf10d0ee0fe7ece
paaverkiev <pa.averkiev@corp.tensor.ru>
2021-05-05 21:13:42 +0300

    https://online.sbis.ru/opendoc.html?guid=4cfadff9-cbb0-4f7c-91b4-0e905636bf43

--- Controls-demo/list_new/Marker/OnBeforeMarkedKeyChanged/Index.ts
+++ Controls-demo/list_new/Marker/OnBeforeMarkedKeyChanged/Index.ts
@@ -7,7 +7,6 @@ import * as Template from 'wml!Controls-demo/list_new/Marker/OnBeforeMarkedKeyCh
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
-    protected _markedKey: CrudEntityKey;
 
     protected _beforeMount(): void {
         this._viewSource = new Memory({
@@ -15,30 +14,26 @@ export default class extends Control {
             data: [
                 {
                     id: 1,
-                    title: 'На записи разрешена установка маркера'
+                    title: 'Асинхронная обработка перед установкой маркера'
                 },
                 {
                     id: 2,
-                    title: 'На записи запрещена установка маркера'
+                    title: 'Асинхронная обработка перед установкой маркера'
                 },
                 {
                     id: 3,
-                    title: 'На записи разрешена установка маркера'
-                },
-                {
-                    id: 4,
-                    title: 'На записи запрещена установка маркера'
-                },
-                {
-                    id: 5,
-                    title: 'На записи разрешена установка маркера'
+                    title: 'Асинхронная обработка перед установкой маркера'
                 }
             ]
         });
     }
 
-    onBeforeMarkedKeyChanged(event: SyntheticEvent, key: CrudEntityKey): CrudEntityKey | Promise<CrudEntityKey> {
-        return key === 2 || key === 4 ? null : key;
+    _onBeforeMarkedKeyChanged(event: SyntheticEvent, key: CrudEntityKey): CrudEntityKey | Promise<CrudEntityKey> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(key);
+            }, 1000);
+        });
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
