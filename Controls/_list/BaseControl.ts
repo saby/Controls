ef3414c9c4c081743beb19caf7fe79848d82b2e2
paaverkiev <pa.averkiev@corp.tensor.ru>
2021-05-12 09:59:45 +0300

    https://online.sbis.ru/opendoc.html?guid=e5385fb8-3c44-4ecb-a2ac-ff65e4c935c7

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -2020,7 +2020,7 @@ const _private = {
     },
 
     openContextMenu(self: typeof BaseControl, event: SyntheticEvent<MouseEvent>, itemData: CollectionItem<Model>): void {
-        if (itemData['[Controls/_display/GroupItem]']) {
+        if (!itemData.ItemActionsItem) {
             return;
         }
 
