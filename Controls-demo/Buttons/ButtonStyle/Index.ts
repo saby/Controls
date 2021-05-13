88661718caf22bc7495c5f94634bdcd029dcc847
ma.mochalov <ma.mochalov@tensor.ru>
2021-05-13 12:11:58 +0300

    https://online.sbis.ru/opendoc.html?guid=87b9837d-eed4-4c03-bd9f-2aef7eb28fbd

--- Controls-demo/Buttons/ButtonStyle/Index.ts
+++ Controls-demo/Buttons/ButtonStyle/Index.ts
@@ -3,7 +3,7 @@ import controlTemplate = require('wml!Controls-demo/Buttons/ButtonStyle/Template
 
 class ButtonStyle extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
-    protected _buttonStyles: string[] = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'unaccented', 'pale'];
+    protected _buttonStyles: string[] = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'unaccented', 'default', 'pale'];
     static _theme: string[] = ['Controls/Classes'];
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
