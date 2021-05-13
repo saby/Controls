cbe4819329045f1637483a5704dbac4320aebc8d
aa.bykanov <aa.bykanov@tensor.ru>
2021-05-13 14:24:15 +0300

    https://online.sbis.ru/opendoc.html?guid=08705217-4c1e-4488-a85b-3c781d0b3e22

--- Controls/gridOld.ts
+++ Controls/gridOld.ts
@@ -10,7 +10,6 @@
  * @includes ResultColumnTemplate Controls/_gridOld/interface/ResultColumnTemplate
  * @includes EditingEmptyTemplate Controls/_gridOld/interface/EditingEmptyTemplate
  * @includes FooterTemplate Controls/_gridOld/interface/FooterTemplate
- * @includes EmptyTemplate Controls/_gridOld/interface/EmptyTemplate
  * @includes RowEditor Controls/_gridOld/interface/RowEditor
  * @includes IPropStorage Controls/_gridOld/interface/IPropStorage
  * @includes ITagColumn Controls/_gridOld/interface/ITagColumn
