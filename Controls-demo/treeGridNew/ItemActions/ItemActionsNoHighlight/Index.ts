4348720d854297b57ffaafe7a7905316ad8b4a3d
pa.averkiev <pa.averkiev@tensor.ru>
2021-05-18 13:28:55 +0300

    https://online.sbis.ru/opendoc.html?guid=69544802-eb74-47fb-86d1-8dbc57e24d0d

--- Controls-demo/treeGridNew/ItemActions/ItemActionsNoHighlight/Index.ts
+++ Controls-demo/treeGridNew/ItemActions/ItemActionsNoHighlight/Index.ts
@@ -1,6 +1,7 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/treeGridNew/ItemActions/ItemActionsNoHighlight/ItemActionsNoHighlight';
 import {Memory} from 'Types/source';
+import {Model} from 'Types/entity';
 import {Gadgets} from '../../DemoHelpers/DataCatalog';
 import {getActionsForContacts as getItemActions} from '../../../list_new/DemoHelpers/ItemActionsCatalog';
 import { IColumn } from 'Controls/grid';
@@ -9,6 +10,8 @@ import { IItemAction } from 'Controls/itemActions';
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
+    protected _highlightOnHover: boolean;
+    protected _expandedItems: any[] = [];
     protected _columns: IColumn[] = Gadgets.getColumnsWithFixedWidth().map((cur, i) => {
         // tslint:disable-next-line
         if (i === 2) {
@@ -26,6 +29,10 @@ export default class extends Control {
             keyProperty: 'id',
             data: Gadgets.getFlatData()
         });
+    }
+
+    protected _getHighlightOnHover(item: Model) {
+        return item.getContents().getKey !== undefined && !this._expandedItems.includes(item.getContents().getKey());
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
