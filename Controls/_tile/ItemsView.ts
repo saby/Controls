fdae34220521e4355c1db2487219f14e395051eb
ka.panihin <ka.panihin@tensor.ru>
2021-05-18 22:59:38 +0300

    https://online.sbis.ru/opendoc.html?guid=46a18a43-f1a3-4eac-b345-04ea12c4c948

--- Controls/_tile/ItemsView.ts
+++ Controls/_tile/ItemsView.ts
@@ -4,4 +4,20 @@ import TileView from 'Controls/_tile/TileView';
 export default class ItemsView extends BaseItemsView {
     protected _viewName: Function = TileView;
     protected _viewModelConstructor: string = 'Controls/tile:TileCollection';
+
+    static getDefaultOptions(): object {
+        return {
+            actionAlignment: 'vertical',
+            actionCaptionPosition: 'none'
+        };
+    }
 }
+
+Object.defineProperty(ItemsView, 'defaultProps', {
+    enumerable: true,
+    configurable: true,
+
+    get(): object {
+        return ItemsView.getDefaultOptions();
+    }
+});
