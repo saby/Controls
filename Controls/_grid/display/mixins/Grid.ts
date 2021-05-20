933adf73dc6255dfb21b8c99602544c80a383ed5
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-05-20 14:18:55 +0300

    https://online.sbis.ru/opendoc.html?guid=292cde2a-fef6-483a-9861-247ab35abb56

--- Controls/_grid/display/mixins/Grid.ts
+++ Controls/_grid/display/mixins/Grid.ts
@@ -301,6 +301,7 @@ export default abstract class Grid<S, T extends GridRowMixin<S>> {
         // Например, задав columns=[{},{}] и footerTemplate=function(){}, то должен создаваться класс Row с опциями
         // columnsConfig=[{}, {}] и columns=[{ template: function(){} }].
         this.getFooter()?.resetColumns();
+        this.getEmptyGridRow()?.resetColumns();
     }
 
     setLadderProperties(ladderProperties: string[]) {
@@ -358,6 +359,16 @@ export default abstract class Grid<S, T extends GridRowMixin<S>> {
             rowTemplateOptions: this._$emptyTemplateOptions,
             multiSelectVisibility: this._$multiSelectVisibility
         });
+    }
+
+    setEmptyTemplateColumns(emptyTemplateColumns): void {
+        this._$emptyTemplateColumns = emptyTemplateColumns;
+        this._nextVersion();
+        if (this._$emptyGridRow) {
+            this._$emptyGridRow.setColumns(emptyTemplateColumns);
+        } else {
+            this._initializeEmptyRow();
+        }
     }
 
     protected _prepareLadder(ladderProperties: string[], columns: TColumns): void {
