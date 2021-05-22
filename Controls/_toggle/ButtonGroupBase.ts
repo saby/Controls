38ab9115cad27c02efddf9eddbb95ac04177e5de
ma.mochalov <ma.mochalov@tensor.ru>
2021-05-22 12:07:13 +0300

    https://online.sbis.ru/opendoc.html?guid=ae723e6a-feb5-4fc6-996e-efc38000f296

--- Controls/_toggle/ButtonGroupBase.ts
+++ Controls/_toggle/ButtonGroupBase.ts
@@ -7,12 +7,22 @@ import 'css!Controls/toggle';
 import 'css!Controls/CommonClasses';
 
 export interface IButtonGroupOptions extends ISingleSelectableOptions, IControlOptions, IItemsOptions<object> {
+    allowMultiselection?: boolean;
 }
+
+/**
+ * @name Controls/_toggle/ButtonGroupBase#allowMultiselection
+ * @cfg {Boolean} Использование единичного выбора с возможностью сбросить значение.
+ * @default false
+ * @demo Controls-demo/toggle/ButtonGroup/AllowMultiselection/Index
+ */
+
 /**
  * @class Controls/_toggle/ButtonGroupBase
  * @extends UI/Base:Control
  * @mixes Controls/interface:ISingleSelectable
  * @mixes Controls/interface:IItems
+ * @mixes Controls/toggle:IButtonGroupOptions
  *
  * @public
  * @author Красильников А.С.
@@ -25,12 +35,15 @@ class ButtonGroupBase extends Control<IButtonGroupOptions> {
         const isNewItemSelected = item.get(keyProperty) !== this._options.selectedKey;
         if (!this._options.readOnly && isNewItemSelected) {
             this._notify('selectedKeyChanged', [item.get(keyProperty)]);
+        } else if (!isNewItemSelected && this._options.allowMultiselection) {
+            this._notify('selectedKeyChanged', [null]);
         }
     }
 
     static getDefaultOptions(): IButtonGroupOptions {
         return {
-            keyProperty: 'id'
+            keyProperty: 'id',
+            allowMultiselection: false
         };
     }
 }
---
+++ Controls/_toggle/Chips.ts
@@ -0,0 +1,54 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import {IItemsOptions, IMultiSelectableOptions} from 'Controls/interface';
+import {ButtonTemplate} from 'Controls/buttons';
+import {Model} from 'Types/entity';
+import {SyntheticEvent} from 'Vdom/Vdom';
+import * as template from 'wml!Controls/_toggle/Chips/Chips';
+
+export interface IChipsOptions extends IMultiSelectableOptions, IControlOptions, IItemsOptions<object> {
+}
+
+/**
+ * Контрол представляет собой набор из нескольких взаимосвязанных между собой кнопок. Используется, когда необходимо выбрать несколько параметров.
+ * @class Controls/_toggle/Chips
+ * @extends UI/Base:Control
+ * @mixes Controls/interface:IMultiSelectable
+ * @mixes Controls/interface:IItems
+ * @public
+ * @author Красильников А.С.
+ * @demo Controls-demo/toggle/Chips/Index
+ */
+
+class Chips extends Control<IChipsOptions> {
+    protected _template: TemplateFunction = template;
+    protected _buttonTemplate: TemplateFunction = ButtonTemplate;
+
+    protected _onItemClick(event: SyntheticEvent<Event>, item: Model): void {
+        const keyProperty = this._options.keyProperty;
+        const selectedKeys = this._options.selectedKeys;
+        const itemIndex = selectedKeys.indexOf(item.get(keyProperty));
+        if (itemIndex === -1) {
+            selectedKeys.push(item.get(keyProperty));
+        } else {
+            selectedKeys.splice(itemIndex, 1);
+        }
+        this._notify('selectedKeysChanged', [selectedKeys]);
+    }
+
+    static getDefaultOptions(): IChipsOptions {
+        return {
+            keyProperty: 'id'
+        };
+    }
+}
+
+Object.defineProperty(Chips, 'defaultProps', {
+    enumerable: true,
+    configurable: true,
+
+    get(): object {
+        return Chips.getDefaultOptions();
+    }
+});
+
+export default Chips;
