e430f0fcbd191ec9966e539bdac0f6cddd24a05c
ma.mochalov <ma.mochalov@tensor.ru>
2021-05-26 18:15:24 +0300

    https://online.sbis.ru/opendoc.html?guid=843f44c6-0442-4919-9d3a-c00ec63c2a3a

--- Controls/_dropdown/Button.ts
+++ Controls/_dropdown/Button.ts
@@ -25,6 +25,7 @@ export interface IButtonOptions extends IBaseDropdownOptions, IIconOptions, IHei
     fontSize?: string;
     showHeader?: boolean;
     menuPopupTrigger?: 'click' | 'hover';
+    isAutoItemClick?: boolean;
 }
 
 /**
@@ -190,7 +191,9 @@ export default class Button extends BaseDropdown {
         }
         if (this._calmTimer.isStarted()) {
             if (this._controller.getItems() && this._controller.getItems().getCount()) {
-                this._onItemClickHandler([this._controller.getItems().at(0)]);
+                if (this._options.isAutoItemClick !== false) {
+                    this._onItemClickHandler([this._controller.getItems().at(0)]);
+                }
                 this._calmTimer.stop();
             }
         } else {
