64b0ad4b4013beb9bba78011848beed7b3f63f30
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-05-26 16:17:51 +0300

    https://online.sbis.ru/opendoc.html?guid=e75572de-85fc-43df-9606-4c8a89b177ff

--- tests/ControlsUnit/grid_clean/GridViewTable.test.ts
+++ tests/ControlsUnit/grid_clean/GridViewTable.test.ts
@@ -15,24 +15,12 @@ describe('Controls/grid_clean/GridViewTable', () => {
         options = {};
     });
 
-    describe('_getGridViewClasses', () => {
-        it('without columnScroll', () => {
-            gridView = new GridViewTable(options);
-            gridView._listModel = listModel;
-            assertClasses.include(
-                gridView._getGridViewClasses(options),
-                'controls-Grid_table-layout controls-Grid_table-layout_fixed'
-            );
-        });
-
-        it('with columnScroll', () => {
-            options = {...options, columnScroll: true};
-            gridView = new GridViewTable(options);
-            gridView._listModel = listModel;
-            assertClasses.include(
-                gridView._getGridViewClasses(options),
-                'controls-Grid_table-layout controls-Grid_table-layout_auto'
-            );
-        });
+    it('_getGridViewClasses', () => {
+        gridView = new GridViewTable(options);
+        gridView._listModel = listModel;
+        assertClasses.include(
+            gridView._getGridViewClasses(options),
+            'controls-Grid_table-layout controls-Grid_table-layout_fixed'
+        );
     });
 });
