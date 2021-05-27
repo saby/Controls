c364dc346b2188f9a85c7d0b73c2672237337dce
pyankov.sa <p_stas1997@mail.ru>
2021-05-27 18:35:45 +0700

    Update ContainerBase.ts
    https://online.sbis.ru/opendoc.html?guid=6221384e-f4cd-4b97-a836-3feb1f257236

--- Controls/_scroll/ContainerBase.ts
+++ Controls/_scroll/ContainerBase.ts
@@ -99,7 +99,7 @@ export default class ContainerBase<T extends IContainerBaseOptions> extends Cont
         // может быть сразу проскролен. Исправляем эту ситуацию.
         // Не будем скроллить в случае, если на странице есть нативные якоря для скролла,
         // т.е. в ссылке присутсвует хэш
-        if (!location.hash) {
+        if (!location.hash || this._container.dataset.scrollContainerNode) {
             this._children.content.scrollTop = 0;
         }
     }
@@ -167,6 +167,9 @@ export default class ContainerBase<T extends IContainerBaseOptions> extends Cont
         this._scrollModel = null;
         this._oldScrollState = null;
         this._isUnmounted = true;
+        // Установим дата аттрибут, чтобы в будущем была возможность определить, был ли в этой ноде скролл контейнер.
+        // Подробности в комментарии в _componentDidMount.
+        this._container.dataset.scrollContainerNode = 'true';
     }
 
     _controlResizeHandler(): void {
