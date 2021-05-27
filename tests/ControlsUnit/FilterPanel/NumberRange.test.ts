8ba134aad86ee53d9ff422a7e6be595f2a58f145
ea.melnikova <ea.melnikova@tensor.ru>
2021-05-27 08:52:18 +0300

    https://online.sbis.ru/opendoc.html?guid=df0a019f-d354-4380-9eb1-c777c533ac8d

--- tests/ControlsUnit/FilterPanel/NumberRange.test.ts
+++ tests/ControlsUnit/FilterPanel/NumberRange.test.ts
@@ -7,6 +7,11 @@ describe('Controls/filterPanel:NumberRangeEditor', () => {
         const numberRangeEditor = new NumberRangeEditor({});
         let changesNotified = false;
         let textValue = null;
+        const event = {
+            target: {
+                closest: () => {}
+            }
+        };
         numberRangeEditor._notify = (eventName, extendedValue) => {
             textValue = extendedValue[0].textValue;
             changesNotified = true;
@@ -14,14 +19,14 @@ describe('Controls/filterPanel:NumberRangeEditor', () => {
 
         it('minValue is null', () => {
             numberRangeEditor._maxValue = 5;
-            numberRangeEditor._handleInputCompleted(null, 1);
+            numberRangeEditor._handleInputCompleted(event, 1);
             assert.equal(textValue, '');
             assert.isTrue(changesNotified);
         });
 
         it('minValue is 0', () => {
             numberRangeEditor._minValue = 0;
-            numberRangeEditor._handleInputCompleted(null, 0);
+            numberRangeEditor._handleInputCompleted(event, 0);
             assert.equal(textValue, '0 - 5');
             assert.isTrue(changesNotified);
         });
@@ -29,7 +34,7 @@ describe('Controls/filterPanel:NumberRangeEditor', () => {
         it('maxValue is less than minValue', () => {
             numberRangeEditor._minValue = 10;
             numberRangeEditor._maxValue = 1;
-            numberRangeEditor._handleInputCompleted(null, 1);
+            numberRangeEditor._handleInputCompleted(event, 1);
             assert.equal(numberRangeEditor._minValue, 1);
             assert.equal(numberRangeEditor._maxValue, 10);
         });
@@ -37,15 +42,20 @@ describe('Controls/filterPanel:NumberRangeEditor', () => {
 
     describe('_handleMinValueChanged', () => {
         const numberRangeEditor = new NumberRangeEditor({});
+        const event = {
+            target: {
+                closest: () => {}
+            }
+        };
 
         it('minValue is less than maxValue', () => {
             numberRangeEditor._maxValue = 5;
-            numberRangeEditor._handleMinValueChanged(null, 1);
+            numberRangeEditor._handleMinValueChanged(event, 1);
             assert.equal(numberRangeEditor._minValue, 1);
         });
 
         it('minValue is bigger than maxValue', () => {
-            numberRangeEditor._handleInputCompleted(null, 16);
+            numberRangeEditor._handleInputCompleted(event, 16);
             assert.equal(numberRangeEditor._minValue, 1);
         });
     });
@@ -53,16 +63,21 @@ describe('Controls/filterPanel:NumberRangeEditor', () => {
     describe('_notifyExtendedValue', () => {
         const numberRangeEditor = new NumberRangeEditor({});
         let changesNotified = false;
+        const event = {
+            target: {
+                closest: () => {}
+            }
+        };
         numberRangeEditor._notify = () => {
             changesNotified = true;
         };
         it('minValue is bigger than maxValue', () => {
-            numberRangeEditor._notifyExtendedValue([4, 3]);
+            numberRangeEditor._notifyExtendedValue(event, [4, 3]);
             assert.isFalse(changesNotified);
         });
 
         it('minValue is less than maxValue', () => {
-            numberRangeEditor._notifyExtendedValue([3, 4]);
+            numberRangeEditor._notifyExtendedValue(event, [3, 4]);
             assert.isTrue(changesNotified);
         });
     });
