161af354b978377291933c3084f4e7269fc71be8
Jaroslav <60604765+YanSirotkin@users.noreply.github.com>
2021-05-28 12:55:09 +0300

    Update IitemEditorTemplate.ts

--- Controls/_grid/interface/IitemEditorTemplate.ts
+++ Controls/_grid/interface/IitemEditorTemplate.ts
@@ -2,7 +2,6 @@
  * Шаблон, который используется для отображения строки {@link Controls/grid:View} без деления на ячейки в {@link /doc/platform/developmentapl/interface-development/controls/list/actions/edit/template/#item-editor-template режиме редактирования}.
  * @class Controls/_grid/interface/IitemEditorTemplate
  * @author Авраменко А.С.
- * @see Controls/treeGrid:View#
  * @example
  * <pre class="brush: html; highlight: [3-10]">
  * <!-- WML -->
