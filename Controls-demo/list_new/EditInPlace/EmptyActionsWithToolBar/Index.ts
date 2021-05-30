8274e8f9fa748c732f0987b8dbeba3a7e78ea001
paaverkiev <pa.averkiev@corp.tensor.ru>
2021-05-30 16:10:32 +0300

    https://online.sbis.ru/opendoc.html?guid=4d368c41-60fb-455c-a339-8eead8fe14bf

--- Controls-demo/list_new/EditInPlace/EmptyActionsWithToolBar/Index.ts
+++ Controls-demo/list_new/EditInPlace/EmptyActionsWithToolBar/Index.ts
@@ -2,17 +2,24 @@ import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/list_new/EditInPlace/EmptyActionsWithToolBar/EmptyActionsWithToolBar';
 import {Memory} from 'Types/source';
 import {getFewCategories as getData} from '../../DemoHelpers/DataCatalog';
+import {IEditingConfig} from 'Controls/display';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _viewSource: Memory;
+    private _viewSource: Memory;
+    protected _editingConfig: IEditingConfig = null;
     private _newData: unknown = getData().slice(0, 1);
-
-    protected _beforeMount(): void {
-        this._newData[0].id = null;
+    protected _beforeMount(): Promise<void> {
         this._viewSource = new Memory({
             keyProperty: 'id',
             data: this._newData
+        });
+        return this._viewSource.read(1).then((res) => {
+            this._editingConfig = {
+                toolbarVisibility: true,
+                item: res,
+                editOnClick: true
+            };
         });
     }
 
