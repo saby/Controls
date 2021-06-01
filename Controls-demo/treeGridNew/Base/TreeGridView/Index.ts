3b770407bac689a6d6187bbfc0c045109a52479b
pa.averkiev <pa.averkiev@tensor.ru>
2021-06-01 10:25:12 +0300

    https://online.sbis.ru/opendoc.html?guid=04d12c1e-77fc-4d59-9427-dea984d3af02

--- Controls-demo/treeGridNew/Base/TreeGridView/Index.ts
+++ Controls-demo/treeGridNew/Base/TreeGridView/Index.ts
@@ -1,12 +1,13 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/treeGridNew/Base/TreeGridView/TreeGridView';
 import {Memory} from 'Types/source';
+import {IColumn} from 'Controls/grid';
 import {Gadgets} from '../../DemoHelpers/DataCatalog';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
-    protected _columns: unknown[] = Gadgets.getGridColumnsForFlat();
+    protected _columns: IColumn[] = Gadgets.getGridColumnsForFlat();
 
     protected _beforeMount(): void {
         this._viewSource = new Memory({
