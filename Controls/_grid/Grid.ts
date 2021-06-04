5fefda4079e34509193aefe8013661edb57b7a56
aa.bykanov <aa.bykanov@tensor.ru>
2021-06-04 16:53:28 +0300

    https://online.sbis.ru/opendoc.html?guid=c3f9ea74-87a7-43a2-9eab-a4f01ea718b9

--- Controls/_grid/Grid.ts
+++ Controls/_grid/Grid.ts
@@ -4,6 +4,66 @@ import GridViewTable from 'Controls/_grid/GridViewTable';
 import { isFullGridSupport } from 'Controls/display';
 import { TemplateFunction } from 'UI/Base';
 
+/**
+ * Контрол "Таблица" позволяет отображать данные из различных источников в виде таблицы.
+ * Контрол поддерживает широкий набор возможностей, позволяющих разработчику максимально гибко настраивать отображение данных.
+ *
+ * @remark
+ * Полезные ссылки:
+ * * {@link /doc/platform/developmentapl/interface-development/controls/list/ руководство разработчика}
+ * * {@link https://github.com/saby/wasaby-controls/blob/897d41142ed56c25fcf1009263d06508aec93c32/Controls-default-theme/variables/_grid.less переменные тем оформления grid}
+ * * {@link https://github.com/saby/wasaby-controls/blob/897d41142ed56c25fcf1009263d06508aec93c32/Controls-default-theme/variables/_list.less переменные тем оформления list}
+ *
+ * @class Controls/_grid/Grid
+ * @extends Controls/list:View
+ * @mixes Controls/interface:ISource
+ * @mixes Controls/interface/IPromisedSelectable
+ * @mixes Controls/interface:INavigation
+ * @mixes Controls/interface:IFilterChanged
+ * @mixes Controls/list:IList
+ * @mixes Controls/itemActions:IItemActions
+ * @mixes Controls/interface:IGridControl
+ * @mixes Controls/interface/IGridItemTemplate
+ * @mixes Controls/interface:IDraggable
+ * @mixes Controls/interface/IGroupedGrid
+ * @mixes Controls/interface/IGridItemTemplate
+ * @mixes Controls/grid:IPropStorage
+ * @mixes Controls/marker:IMarkerList
+ *
+ * @public
+ * @author Авраменко А.С.
+ * @demo Controls-demo/grid/Base/Index
+ */
+
+/*
+ * Table-looking list. Can load data from data source.
+ * The detailed description and instructions on how to configure the control you can read <a href='/doc/platform/developmentapl/interface-development/controls/list/'>here</a>.
+ * List of examples:
+ * <ul>
+ *    <li><a href="/materials/Controls-demo/app/Controls-demo%2FList%2FGrid%2FEditableGrid">How to configure editing in your list</a>.</li>
+ * </ul>
+ *
+ * @class Controls/_grid/Grid
+ * @extends Controls/list:View
+ * @mixes Controls/interface:ISource
+ * @mixes Controls/interface/IPromisedSelectable
+ * @mixes Controls/interface/IGroupedGrid
+ * @mixes Controls/interface:INavigation
+ * @mixes Controls/interface:IFilterChanged
+ * @mixes Controls/list:IList
+ * @mixes Controls/itemActions:IItemActions
+ * @mixes Controls/interface:ISorting
+ * @mixes Controls/interface:IGridControl
+ * @mixes Controls/interface/IGridItemTemplate
+ * @mixes Controls/interface:IDraggable
+ * @mixes Controls/grid:IPropStorage
+ * @mixes Controls/marker:IMarkerList
+ *
+ *
+ * @public
+ * @author Авраменко А.С.
+ * @demo Controls-demo/grid/Base/Index
+ */
 export default class Grid extends List {
     protected _viewName: TemplateFunction = null;
     protected _viewTemplate: TemplateFunction = viewTemplate;
