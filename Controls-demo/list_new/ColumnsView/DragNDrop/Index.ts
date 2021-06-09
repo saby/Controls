04f2e4df13c474735b0f3e91ddac7465834130b9
va.kolesov <va.kolesov@tensor.ru>
2021-06-09 16:27:05 +0300

    https://online.sbis.ru/opendoc.html?guid=25dfabfc-abff-4981-ae7a-d9a689e4e2c4

---
+++ Controls-demo/list_new/ColumnsView/DragNDrop/Index.ts
@@ -0,0 +1,49 @@
+import { Control, TemplateFunction } from 'UI/Base';
+import template = require('wml!Controls-demo/list_new/ColumnsView/DragNDrop/Template');
+import {Memory} from 'Types/source';
+import {generateData} from '../../DemoHelpers/DataCatalog';
+import { SyntheticEvent } from 'UI/Vdom';
+import * as Dnd from 'Controls/dragnDrop';
+import {Collection} from 'Controls/display';
+import {Model} from 'Types/entity';
+
+const NUMBER_OF_ITEMS = 25;
+
+export default class RenderDemo extends Control {
+    protected _template: TemplateFunction = template;
+    protected _viewSource: Memory;
+    private _dataArray: Array<{id: number, title: string}>;
+    protected _selectedKeys: Number[] = [];
+    private _multiselect: 'visible'|'hidden' = 'hidden';
+
+    protected _beforeMount(): void {
+        this._dataArray = generateData<{id: number, key: number, title: string}>({
+            count: NUMBER_OF_ITEMS,
+            entityTemplate: {title: 'string'},
+            beforeCreateItemCallback: (item) => {
+                item.title = `Запись с key="${item.id}". `;
+                item.key = item.id;
+            }
+        });
+        this._viewSource = new Memory({
+            data: this._dataArray,
+            keyProperty: 'key'
+        });
+    }
+
+    protected _dragStart(_: SyntheticEvent, items: number[]): Dnd.ItemsEntity {
+        return new Dnd.ItemsEntity({
+            items,
+        });
+    }
+
+    protected _dragEnd(_: SyntheticEvent, entity: Collection<Model>, target: unknown, position: string): void {
+        this._selectedKeys = [];
+        return this._children.listMover.moveItems(entity.getItems(), target, position);
+    }
+    protected _onToggle(): void {
+        this._multiselect = this._multiselect === 'visible' ? 'hidden' : 'visible';
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
