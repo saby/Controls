9cc66cb6f6922e18f0a8398ac72687291aecd3b4
aa.bykanov <aa.bykanov@tensor.ru>
2021-06-16 18:26:57 +0300

    https://online.sbis.ru/opendoc.html?guid=50829607-e197-451f-8563-b47374292ae5

--- Controls/interface/IEditableList.js
+++ Controls/interface/IEditableList.js
@@ -518,11 +518,14 @@ define('Controls/interface/IEditableList', [
     * @param {Controls/interface/IEditableList/ItemEditOptions.typedef} options Параметры редактирования.
     * @returns {Promise<void | {canceled: true}>}
     * @remark
+    * Используйте этот метод в ситуациях, когда вы хотите начать редактирование из нестандартного места, например, из {@link /doc/platform/developmentapl/interface-development/controls/list/actions/operations/ панели действий элемента}.
+    * 
     * Promise разрешается после монтирования контрола в DOM.
+    * Возвращается {canceled: true} в случае отмены запуска редактирования по месту.
     *
     * Перед запуском редактирования по месту происходит событие {@link beforeBeginEdit}, а после запуска — {@link afterBeginEdit}.
     *
-    * Используйте этот метод в ситуациях, когда вы хотите начать редактирование из нестандартного места, например, из {@link /doc/platform/developmentapl/interface-development/controls/list/actions/operations/ панели действий элемента}.
+    * 
     *
     * Формат полей редактируемой записи может отличаться от формата полей Types/Collection:RecordSet, отображаемый списком. Подробнее читайте {@link /doc/platform/developmentapl/interface-development/controls/list/actions/edit/ways-to-start/code/#begin-edit-format здесь}.
     * @example
@@ -576,9 +579,10 @@ define('Controls/interface/IEditableList', [
     * Запускает {@link /doc/platform/developmentapl/interface-development/controls/list/actions/edit/ добавление по месту}.
     * @function Controls/interface/IEditableList#beginAdd
     * @param {Controls/interface/IEditableList/ItemEditOptions.typedef} options Параметры добавления.
-    * @returns {Promise}
+    * @returns {Promise<void | {canceled: true}>}
     * @remark
     * Promise разрешается после монтирования контрола в DOM.
+    * Возвращается {canceled: true} в случае отмены запуска добавления по месту.
     * 
     * Перед запуском добавления по месту происходит событие {@link Controls/interface/IEditableList#beforeBeginEdit beforeBeginEdit}, а после запуска — {@link Controls/interface/IEditableList#afterBeginEdit afterBeginEdit}.
     *
@@ -613,7 +617,7 @@ define('Controls/interface/IEditableList', [
     * Starts adding.
     * @function Controls/interface/IEditableList#beginAdd
     * @param {Controls/interface/IEditableList/ItemEditOptions.typedef} options Options of adding.
-    * @returns {Promise}
+    * @returns {Promise<void | {canceled: true}>}
     * @remark
     * If you don't pass the options then {@link Types/source:ICrud#create create} method of the list's source will be called and the result will be added to the list.
     * @example
@@ -665,7 +669,7 @@ define('Controls/interface/IEditableList', [
    /*
     * Ends editing and commits changes.
     * @function Controls/interface/IEditableList#commitEdit
-    * @returns {Promise}
+    * @returns {Promise<void | {canceled: true}>}
     * @remark
     * Use this method when you want to end editing in response to user action, e.g., when a user tries to close a dialog you'd use this method to save changes.
     * @example
@@ -688,11 +692,12 @@ define('Controls/interface/IEditableList', [
    /**
     * Завершает {@link /doc/platform/developmentapl/interface-development/controls/list/actions/edit/ редактирование/добавление по месту} без сохранения введенных данных.
     * @function Controls/interface/IEditableList#cancelEdit
-    * @returns {Promise}
+    * @returns {Promise<void | {canceled: true}>}
     * @remark
-    * Promise разрешается после монтирования контрола в DOM.
-    * 
     * Используйте этот метод, когда вы хотите завершить редактирование или добавление в ответ на действия пользователя, например, когда пользователь нажимает на кнопку "Отмена".
+    * 
+    * Promise разрешается после монтирования контрола в DOM.
+    * Возвращается {canceled: true} в случае отмены завершения редактирование/добавление по месту без сохранения введенных данных.
     *
     * При завершении редактирования по месту происходят события, подробнее о которых читайте {@link /doc/platform/developmentapl/interface-development/controls/list/actions/edit/events/ здесь}.
     * @example
@@ -715,7 +720,7 @@ define('Controls/interface/IEditableList', [
    /*
     * Ends editing and discards changes.
     * @function Controls/interface/IEditableList#cancelEdit
-    * @returns {Promise}
+    * @returns {Promise<void | {canceled: true}>}
     * @remark
     * Use this method when you want to end editing in response to user action, e.g., when a user clicks on a 'Cancel' button.
     * @example
