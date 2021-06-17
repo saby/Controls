78ac60aa65f69ec8ef6314fa5353791f2ad968dd
pa.averkiev <pa.averkiev@tensor.ru>
2021-06-17 09:00:37 +0300

    https://online.sbis.ru/opendoc.html?guid=4de08dde-fbda-4f78-acd7-89790aa16cfc

--- Controls/_moverDialog/Template.ts
+++ Controls/_moverDialog/Template.ts
@@ -29,7 +29,7 @@ export interface IMoverDialogTemplateOptions extends IControlOptions {
     parentProperty: string;
     filter?: QueryWhereExpression<unknown>;
     headingCaption?: string;
-    rootTitle?: string;
+    rootTitle: string;
     rootLabelVisible?: boolean;
 }
 
@@ -83,10 +83,16 @@ export default class extends Control<IMoverDialogTemplateOptions> {
         // Выписана задача: https://online.sbis.ru/opendoc.html?guid=aeaff20a-ee07-4d1b-8a9d-2528a269bc91
         this._columns = options.columns.slice();
         this._columns[0].textOverflow = 'ellipsis';
-        this._columns[0].template = cellTemplate;
-        this._columns[0].templateOptions = {
-            rootLabelVisible: options.rootLabelVisible !== false
-        };
+
+        if (options.rootVisible || options.showRoot) {
+            if (!options.rootTitle) {
+                Logger.error('MoverDialog: Для диалога перемещения необходимо указать опцию rootTitle', this);
+            }
+            this._columns[0].template = cellTemplate;
+            this._columns[0].templateOptions = {
+                rootLabelVisible: options.rootLabelVisible !== false
+            };
+        }
 
         this._onItemClick = this._onItemClick.bind(this);
         this._itemsFilterMethod = this._itemsFilterMethod.bind(this);
@@ -142,7 +148,8 @@ export default class extends Control<IMoverDialogTemplateOptions> {
     protected _dataLoadCallback(recordSet: RecordSet): void {
         if ((!this._searchValue || this._searchValue.length === 0) &&
             this._root === this._options.root &&
-            (this._options.showRoot || this._options.rootVisible)) {
+            (this._options.showRoot || this._options.rootVisible) &&
+            !!this._options.rootTitle) {
             recordSet.add(new Model({
                 keyProperty: recordSet.getKeyProperty(),
                 rawData: this._getRootRawData()
