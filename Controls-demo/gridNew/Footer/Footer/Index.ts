33a9e4ddbbdf154c3f5026168eef37171a146ae0
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-06-20 16:17:22 +0300

    https://online.sbis.ru/opendoc.html?guid=a973ff36-e98f-43f7-9a0b-d0c6a34d780d

--- Controls-demo/gridNew/Footer/Footer/Index.ts
+++ Controls-demo/gridNew/Footer/Footer/Index.ts
@@ -1,7 +1,6 @@
 // tslint:disable-next-line:ban-ts-ignore
 // @ts-ignore
-import * as Template from 'wml!Controls-demo/gridNew/Footer/TemplateOptions/Index';
-import 'wml!Controls-demo/gridNew/Footer/TemplateOptions/FooterCellTemplate';
+import * as Template from 'wml!Controls-demo/gridNew/Footer/Footer/Footer';
 import {Control, TemplateFunction} from 'UI/Base';
 import { IColumn } from 'Controls/grid';
 import { Countries } from 'Controls-demo/gridNew/DemoHelpers/Data/Countries';
@@ -10,22 +9,13 @@ import {Memory} from 'Types/source';
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
-    protected _columns: IColumn[] = Countries.getColumns().slice(0, 4);
+    protected _columns: IColumn[] = Countries.getColumnsWithFixedWidths().slice(0, 4);
     protected _footerCfg: unknown[] = [];
 
     protected _beforeMount(): void {
         this._viewSource = new Memory({
             keyProperty: 'key',
             data: Countries.getData().slice(0, 7)
-        });
-
-        this._columns.forEach((c, index) => {
-            this._footerCfg.push({
-                template: 'wml!Controls-demo/gridNew/Footer/TemplateOptions/FooterCellTemplate',
-                templateOptions: {
-                    myVar: index
-                }
-            });
         });
     }
 
--- Controls-demo/gridNew/Footer/FooterTemplate/Index.ts
+++ Controls-demo/gridNew/Footer/FooterTemplate/Index.ts
@@ -1,31 +1,19 @@
-// tslint:disable-next-line:ban-ts-ignore
-// @ts-ignore
-import * as Template from 'wml!Controls-demo/gridNew/Footer/TemplateOptions/Index';
-import 'wml!Controls-demo/gridNew/Footer/TemplateOptions/FooterCellTemplate';
 import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/gridNew/Footer/FooterTemplate/FooterTemplate';
+import {Memory} from 'Types/source';
 import { IColumn } from 'Controls/grid';
 import { Countries } from 'Controls-demo/gridNew/DemoHelpers/Data/Countries';
-import {Memory} from 'Types/source';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
     protected _columns: IColumn[] = Countries.getColumns().slice(0, 4);
-    protected _footerCfg: unknown[] = [];
 
     protected _beforeMount(): void {
+        this._columns[0].width = '30px';
         this._viewSource = new Memory({
             keyProperty: 'key',
             data: Countries.getData().slice(0, 7)
-        });
-
-        this._columns.forEach((c, index) => {
-            this._footerCfg.push({
-                template: 'wml!Controls-demo/gridNew/Footer/TemplateOptions/FooterCellTemplate',
-                templateOptions: {
-                    myVar: index
-                }
-            });
         });
     }
 
