ddb8a3b824ee250564383449ddb64bc97c90d886
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-06-21 13:05:12 +0300

    https://online.sbis.ru/opendoc.html?guid=bea1a61d-accd-40b5-a78b-af0868b39fb9

--- Controls-demo/gridNew/Footer/FooterTemplate/Index.ts
+++ Controls-demo/gridNew/Footer/FooterTemplate/Index.ts
@@ -4,16 +4,19 @@ import {Memory} from 'Types/source';
 import { IColumn } from 'Controls/grid';
 import { Countries } from 'Controls-demo/gridNew/DemoHelpers/Data/Countries';
 
+const COLUMNS_COUNT = 4;
+const DATA_ITEMS_COUNT = 7;
+
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
-    protected _columns: IColumn[] = Countries.getColumns().slice(0, 4);
+    protected _columns: IColumn[] = Countries.getColumns(COLUMNS_COUNT);
 
     protected _beforeMount(): void {
         this._columns[0].width = '30px';
         this._viewSource = new Memory({
             keyProperty: 'key',
-            data: Countries.getData().slice(0, 7)
+            data: Countries.getData(DATA_ITEMS_COUNT)
         });
     }
 
