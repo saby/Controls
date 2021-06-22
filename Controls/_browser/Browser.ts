677a481cc22e950c60e96395e89c581a935612f1
am.gerasimov <am.gerasimov@tensor.ru>
2021-06-22 17:38:56 +0300

    https://online.sbis.ru/opendoc.html?guid=a74d91b6-5c64-47b8-a7ba-192d0ba27368

--- Controls/_browser/Browser.ts
+++ Controls/_browser/Browser.ts
@@ -116,6 +116,7 @@ export default class Browser extends Control<IBrowserOptions, TReceivedState> {
 
     protected _groupHistoryId: string;
     private _errorRegister: RegisterClass;
+    private _rootChangedRegister: RegisterClass;
     private _storeCallbackIds: string[];
     private _storeCtxCallbackId: string;
 
@@ -457,6 +458,13 @@ export default class Browser extends Control<IBrowserOptions, TReceivedState> {
         return this._errorRegister;
     }
 
+    private _getRootChangedRegister(): RegisterClass {
+        if (!this._rootChangedRegister) {
+            this._rootChangedRegister = new RegisterClass({register: 'rootChanged'});
+        }
+        return this._rootChangedRegister;
+    }
+
     private _setItemsAndUpdateContext(): void {
         this._updateItemsOnState();
         this._getSourceController().subscribe('rootChanged', this._rootChanged.bind(this));
@@ -522,6 +530,11 @@ export default class Browser extends Control<IBrowserOptions, TReceivedState> {
         this._dataLoader.getFilterController()?.setFilter(filter);
         this._filter = filter;
         this._notify('filterChanged', [this._filter]);
+    }
+
+    protected _breadCrumbsItemClick(event: SyntheticEvent, root: Key): void {
+        this._rootChanged(event, root);
+        this._getRootChangedRegister().start(root);
     }
 
     protected _rootChanged(event: SyntheticEvent, root: Key, id?: string): void {
@@ -609,14 +622,16 @@ export default class Browser extends Control<IBrowserOptions, TReceivedState> {
     }
 
     protected _registerHandler(event: Event, registerType: string,
-                               component: any, callback: Function, config: object): void {
+                               component: unknown, callback: Function, config: object): void {
         this._getErrorRegister().register(event, registerType, component, callback, config);
         this._getOperationsController().registerHandler(event, registerType, component, callback, config);
+        this._getRootChangedRegister().register(event, registerType, component, callback, config);
     }
 
-    protected _unregisterHandler(event: Event, registerType: string, component: any, config: object): void {
+    protected _unregisterHandler(event: Event, registerType: string, component: unknown, config: object): void {
         this._getErrorRegister().unregister(event, registerType, component, config);
         this._getOperationsController().unregisterHandler(event, registerType, component, config);
+        this._getRootChangedRegister().unregister(event, registerType, component, config);
     }
 
     protected _selectedTypeChangedHandler(event: SyntheticEvent<null>, typeName: string, limit?: number): void {
