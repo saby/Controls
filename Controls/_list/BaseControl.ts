0a1e6050a9e98413e89e36bc4b14d44fa3179c91
ka.panihin <ka.panihin@tensor.ru>
2021-06-22 15:50:56 +0300

    https://online.sbis.ru/opendoc.html?guid=103aa3e3-5ef7-43ab-9a8d-8dc1b5fb689e

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -4599,7 +4599,7 @@ export default class BaseControl<TOptions extends IBaseControlOptions = IBaseCon
         if (this._options.itemsDragNDrop) {
             const container = this._container[0] || this._container;
             container.removeEventListener('dragstart', this._nativeDragStart);
-            this._dragNDropEnded();
+            this._notify('_removeDraggingTemplate', [], {bubbling: true});
         }
         if (this._finishScrollToEdgeOnDrawItems) {
             this._finishScrollToEdgeOnDrawItems = null;
@@ -7130,9 +7130,10 @@ export default class BaseControl<TOptions extends IBaseControlOptions = IBaseCon
         return result;
     }
 
-    _dragNDropEnded(event?: SyntheticEvent): void {
+    _dragNDropEnded(event: SyntheticEvent): void {
         if (this._dndListController && this._dndListController.isDragging()) {
-            this._notify('_documentDragEnd', [this._getDragObject(event?.nativeEvent, this._startEvent)], {bubbling: true});
+            const dragObject = this._getDragObject(event.nativeEvent, this._startEvent);
+            this._notify('_documentDragEnd', [dragObject], {bubbling: true});
         }
         if (this._startEvent && this._startEvent.target) {
             this._startEvent.target.classList.remove('controls-DragNDrop__dragTarget');
