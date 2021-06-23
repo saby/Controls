b18251875298dbfc9aebf032179dea8e2e30c85f
pa.averkiev <pa.averkiev@tensor.ru>
2021-06-23 15:31:32 +0300

    https://online.sbis.ru/opendoc.html?guid=9cd06edc-d7ef-402e-b900-4cd29efacb53

--- Controls-demo/list_new/ItemClick/Base/Index.ts
+++ Controls-demo/list_new/ItemClick/Base/Index.ts
@@ -3,8 +3,8 @@ import * as Template from 'wml!Controls-demo/list_new/ItemClick/Base/ItemClick';
 import {Memory} from 'Types/source';
 import {Model} from 'Types/entity';
 import {SyntheticEvent} from 'Vdom/Vdom';
-import {getActionsForContacts as getItemActions} from '../DemoHelpers/ItemActionsCatalog';
-import {getContactsCatalog} from '../DemoHelpers/DataCatalog';
+import {getActionsForContacts as getItemActions} from '../../DemoHelpers/ItemActionsCatalog';
+import {getContactsCatalog} from '../../DemoHelpers/DataCatalog';
 import {IItemAction} from 'Controls/itemActions';
 
 export default class extends Control {
