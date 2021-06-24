14e8bb5cb1de5cde31fd4beb815c8cad994ef415
ea.melnikova <ea.melnikova@tensor.ru>
2021-06-24 11:44:48 +0300

    https://online.sbis.ru/opendoc.html?guid=6a14f7f7-b650-4122-8780-a296ff966eb2

--- tests/ControlsUnit/FilterPanel/ViewModel.test.ts
+++ tests/ControlsUnit/FilterPanel/ViewModel.test.ts
@@ -78,4 +78,31 @@ describe('Controls/filterPanel:ViewModel', () => {
             assert.equal(item.textValue, 'New text owner');
         });
     });
+
+    describe('setEditingObject', () => {
+        const source = [
+            {
+                group: 'owners',
+                name: 'owner',
+                value: 'Test owner',
+                textValue: 'Test owner',
+                resetValue: null
+            }
+        ];
+        const collapsedGroups = [];
+        const viewModel = new ViewModel({
+            source,
+            collapsedGroups
+        });
+
+        it('collapsed groups reseted', () => {
+            const editingObject = {
+                owner: null
+            };
+            viewModel._collapsedGroups = ['owners'];
+            viewModel._source[0].value = null;
+            viewModel.setEditingObject(editingObject);
+            assert.isEmpty(viewModel._collapsedGroups);
+        });
+    });
 });
