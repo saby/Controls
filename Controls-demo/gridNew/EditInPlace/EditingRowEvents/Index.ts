ad4c83cefa488549436fcf0e1ae877d44666546d
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-06-28 11:09:26 +0300

    https://online.sbis.ru/doc/6e18b2f9-2c9e-4fc1-b04c-234065106ebb

--- Controls-demo/gridNew/EditInPlace/EditingRowEvents/Index.ts
+++ Controls-demo/gridNew/EditInPlace/EditingRowEvents/Index.ts
@@ -1,27 +1,24 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import * as Template from 'wml!Controls-demo/gridNew/EditInPlace/EditingRow/EditingRow';
+import * as Template from 'wml!Controls-demo/gridNew/EditInPlace/EditingRowEvents/EditingRowEvents';
 import {Memory} from 'Types/source';
 import {SyntheticEvent} from 'Vdom/Vdom';
 import {Model} from 'Types/entity';
-import * as cellTemplate from 'wml!Controls-demo/gridNew/EditInPlace/EditingRow/cellTemplate';
-import { Ports } from 'Controls-demo/gridNew/DemoHelpers/Data/Ports';
+import * as cellTemplate from 'wml!Controls-demo/gridNew/EditInPlace/EditingRowEvents/cellTemplate';
+import {Ports} from 'Controls-demo/gridNew/DemoHelpers/Data/Ports';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _viewSource: Memory;
-    protected _documentSignMemory: Memory;
+    private _viewSource: Memory;
+    private _documentSignMemory: Memory;
+    private selectedKey: number = 1;
+    private _fakeId: number = 100;
+    private _cellTemplate: TemplateFunction;
+    private _mouseDownLog: string[] = [];
     private data: object[] = Ports.getData().map((cur) => this.getData(cur));
-    protected selectedKey: number = 1;
-    protected _fakeId: number = 100;
 
     private getData(data: object): object {
         for (const key in data) {
-            if (data[key]) {
-                data[key] = '' + data[key];
-            } else {
-                data[key] = '';
-            }
-
+            data[key] = `${data[key]}` || '';
         }
         return data;
     }
@@ -40,7 +37,7 @@ export default class extends Control {
         });
     }
 
-    protected _colspanCallback(item, column, columnIndex, isEditing) {
+    protected _colspanCallback(item, column, columnIndex, isEditing): 'end' {
         if (isEditing && columnIndex === 0) {
             return 'end';
         }
@@ -69,12 +66,12 @@ export default class extends Control {
         }
     }
 
-    private onChange1 = (_: SyntheticEvent, name: string, item: Model, value: number): void => {
-        item.set(name, value);
+    _onMouseDown() {
+        this._mouseDownLog.push('on:mousedown');
     }
 
-    private onChange2 = (_: SyntheticEvent, key: number): void => {
-        this.selectedKey = key;
+    _clearLog = () => {
+        this._mouseDownLog = [];
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
