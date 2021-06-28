2bc9862392f9aaf83eb380cd442de1cdc9a22389
ma.baranov2 <ma.baranov2@tensor.ru>
2021-06-28 15:05:58 +0300

    https://online.sbis.ru/opendoc.html?guid=4989fde4-e92f-4d21-9351-c40cd8f5605c

--- Controls-demo/SVG/Index.ts
+++ Controls-demo/SVG/Index.ts
@@ -1,6 +1,10 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import * as template from 'wml!Controls-demo/SVG/SVG';
+import * as getResourceUrl from 'Core/helpers/getResourceUrl';
+import Env = require('Env/Env');
 
 export default class extends Control {
     protected _template: TemplateFunction = template;
+
+    private _circleUrl: string = getResourceUrl(`${Env.constants.resourceRoot}Controls-demo/SVG/example.svg#circle`);
 }
