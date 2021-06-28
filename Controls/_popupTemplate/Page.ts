9b2d1f2b49289c244f74107c4281ab6abd92c6ab
dv.onischuk <dv.onischuk@tensor.ru>
2021-06-28 18:18:43 +0300

    https://online.sbis.ru/opendoc.html?guid=2fe9edb7-27a8-4a0b-bab7-0a88424c84fd

--- Controls/_popupTemplate/Page.ts
+++ Controls/_popupTemplate/Page.ts
@@ -1,6 +1,16 @@
 import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import * as template from 'wml!Controls/_popupTemplate/Page/Page';
-import {Controller} from 'Controls/popup';
+
+interface IPageTemplateOptions extends IControlOptions {
+    dataLoaderResult: Promise<unknown>;
+    pageTemplate: string;
+    pageTemplateOptions: object;
+}
+
+interface IPrefetchResult {
+    configError: unknown,
+    data: Array<Record<string, unknown>>
+}
 
 /**
  * Контрол, который отвечает за построение шаблона страницы в окне
@@ -10,10 +20,36 @@ import {Controller} from 'Controls/popup';
  * @private
  * @author Онищук Д.В.
  */
-export default class Template extends Control<IControlOptions, void> {
+export default class Template extends Control<IControlOptions> {
     _template: TemplateFunction = template;
-    protected _pageTemplate: string;
-    protected _beforeMount(): void {
-        this._pageTemplate = Controller.getPageTemplate();
+    protected _prefetchData: Record<string, unknown>;
+
+    protected _beforeMount(options: IPageTemplateOptions): void {
+        if (options.dataLoaderResult) {
+            this._processPreloadResult(options.dataLoaderResult);
+        }
+    }
+
+    protected _beforeUpdate(options: IPageTemplateOptions): void {
+        if (options.dataLoaderResult && this._options.dataLoaderResult !== options.dataLoaderResult) {
+            this._prefetchData = null;
+            this._processPreloadResult(options.dataLoaderResult);
+        }
+    }
+
+    private _processPreloadResult(dataLoaderResult: Promise<unknown>): void {
+        dataLoaderResult.then((result) => {
+            this._prefetchData = this._getPrefetchData(result as IPrefetchResult);
+        });
+    }
+
+    private _getPrefetchData(prefetchResult: IPrefetchResult): Record<string, unknown> {
+        if (prefetchResult.data) {
+            const resultsMap = {};
+            prefetchResult.data.forEach((loaderResult) => {
+                resultsMap[loaderResult.id] = loaderResult;
+            });
+            return resultsMap;
+        }
     }
 }
--- Controls/popup.ts
+++ Controls/popup.ts
@@ -43,6 +43,7 @@ export {default as DialogOpener} from './_popup/PopupHelper/Dialog';
 export {default as NotificationOpener} from './_popup/PopupHelper/Notification';
 export {default as SlidingPanelOpener} from './_popup/PopupHelper/SlidingPanel';
 export {default as PageOpener} from './_popup/PopupHelper/Page';
+export {default as PageController} from './_popup/Page/Controller';
 
 export {default as IPopup, IPopupOptions, IPopupItem, IPopupSizes, IPopupPosition, IEventHandlers, IPopupItemInfo} from './_popup/interface/IPopup';
 export {IBasePopupOptions, IBaseOpener} from './_popup/interface/IBaseOpener';
