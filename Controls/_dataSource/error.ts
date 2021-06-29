998e20ea6e653a5c7ccb3c736e1eac1ab33588a3
oa.kashin <oa.kashin@tensor.ru>
2021-06-29 14:53:19 +0300

    Добавлен Controls/_error/Controller для корректного отображения документации. https://online.sbis.ru/opendoc.html?guid=cf7a671a-bb9d-45c9-b229-2126116604f2

--- Controls/_dataSource/error.ts
+++ Controls/_dataSource/error.ts
@@ -3,6 +3,7 @@
  * Компоненты для обработки и отображения ошибок.
  * {@link /doc/platform/developmentapl/interface-development/application-architecture/error-handling/}
  * @library Controls/dataSource:error
+ * @includes Controller Controls/_error/Controller
  * @includes Container Controls/_dataSource/_error/Container
  * @includes DataLoader Controls/_dataSource/_error/DataLoader
  * @includes Mode Controls/_dataSource/_error/Mode
