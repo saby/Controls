220cbe6721758c80fce59022b4fb7167cc5af67c
aa.bykanov <itdell@yandex.ru>
2021-06-29 15:50:42 +0300

    https://online.sbis.ru/opendoc.html?guid=095594f1-bd06-41f7-9390-728d1f556526

--- Controls/_interface/ISorting.ts
+++ Controls/_interface/ISorting.ts
@@ -19,72 +19,29 @@ export default interface ISorting {
  * @name Controls/_interface/ISorting#sorting
  * @cfg {Array.<Object>} Конфигурация {@link /doc/platform/developmentapl/interface-development/controls/list/sorting/ сортировки}.
  * @remark
- * Допустимы значения ASC/DESC.
+ * Допустимы значения направления сортировки ASC/DESC.
  * 
- * Одновременно можно сортировать только по одному полю.
- * 
- * Если в конфигурации ячейки задать свойство sortingProperty, то в шапке таблицы в конкретной ячейке будет отображаться кнопка для изменения сортировки. Клик по кнопке будет менять порядок сортировки элементов на противоположный. При этом элементы будут отсортированы по полю, имя которого указано в свойстве sortingProperty.
+ * Если в {@link /doc/platform/developmentapl/interface-development/controls/list/grid/header/ конфигурации ячейки шапки} задать свойство {@link Controls/grid:IHeaderCell#sortingProperty sortingProperty}, то в шапке таблицы в конкретной ячейке будет отображаться кнопка для изменения сортировки. Подробнее читайте {@link /doc/platform/developmentapl/interface-development/controls/list/grid/header/sorting/ здесь}.
  * 
  * Выбранную сортировку можно сохранять. Для этого используют опцию {@link Controls/grid:IPropStorage#propStorageId propStorageId}.
  * @example
- * В шапке столбца необходимо задать свойство sortingProperty:
- * 
- * <pre class="brush: js; highlight: [15, 19]">
- * // JavaScript
- * _sorting: null
- * _header: null,
- * _columns: null,
- * _beforeMount: function(options) {
- *    this._sorting = [{
- *        price: 'DESC'
- *    }];
- *    this._header = [
- *       {
- *          caption: ''
- *       },
- *       {
- *          caption: 'Цена',
- *          sortingProperty: 'price'
- *       },
- *       {
- *          caption: 'Остаток',
- *          sortingProperty: 'balance'
- *       }
- *    ];
- *    this._columns = [
- *       {
- *          displayProperty: 'name'
- *       },
- *       {
- *          displayProperty: 'price'
- *       },
- *       {
- *          displayProperty: 'balance'
- *       }
- *    ];
+ * <pre class="brush: js; highlight: [2,4]">
+ * // TypeScript
+ * protected _sorting: unknown = [],
+ * protected _beforeMount(): void {
+ *    this._sorting: [{ price: 'DESC' }];
+ *    this._columns = [{displayProperty: 'name'}, { displayProperty: 'price'}, {displayProperty: 'balance'}]
  * }
  * </pre>
  * 
  * И связать опцию sorting со свойством контрола.
  * 
- * <pre class="brush: html; highlight: [6]">
+ * <pre class="brush: html; highlight: [5]">
  * <!-- WML -->
  * <Controls.grid:View
- *    displayProperty="title"
- *    header="{{_header}}"
+ *    source="{{_viewSource}}"
  *    columns="{{_columns}}"
  *    bind:sorting="_sorting" />
  * </pre>
- *
- * Настройка сортировки массива:
- * 
- * <pre class="brush: js">
- * // JavaScript
- * [
- *    { price: 'DESC' }
- * ]
- * </pre>
- * 
- * Дополнительную информацию смотрите в разделе {@link /doc/platform/developmentapl/service-development/service-contract/objects/blmethods/bllist/declr/#javascript декларативный списочный метод}.
  * @see Controls/grid:SortingSelector
  */
