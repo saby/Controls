ee7bce1614b01d6a76d7360ba6a8dbaf9d4bc165
aa.bykanov <itdell@yandex.ru>
2021-06-30 09:10:18 +0300

    https://online.sbis.ru/opendoc.html?guid=0bbbc413-1faf-4a77-8c35-9befb15d0731

--- Controls/menu.ts
+++ Controls/menu.ts
@@ -13,7 +13,7 @@ import ItemTemplate = require('wml!Controls/_menu/Render/itemTemplate');
 import GroupTemplate = require('wml!Controls/_menu/Render/groupTemplate');
 import EmptyTemplate = require('wml!Controls/_menu/Render/empty');
 
-export {default as Control} from 'зControls/_menu/Control';
+export {default as Control} from 'Controls/_menu/Control';
 export {default as Render} from 'Controls/_menu/Render';
 export {default as Popup} from 'Controls/_menu/Popup';
 export {default as HeaderTemplate} from 'Controls/_menu/Popup/headerTemplate';
