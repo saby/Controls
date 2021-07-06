ded00478e1ef3231364044b9d1f5d7ff691ec5eb
va.kolesov <va.kolesov@tensor.ru>
2021-07-06 09:27:01 +0300

    https://online.sbis.ru/opendoc.html?guid=83c128f8-253e-495f-b83c-25d21436fb23

--- Controls/_list/ScrollController.ts
+++ Controls/_list/ScrollController.ts
@@ -731,7 +731,7 @@ export default class ScrollController {
     }
 
     handleResetItems(): IScrollControllerResult {
-        return this._initVirtualScroll({...this._options, forceInitVirtualScroll: true});
+        return this._initVirtualScroll(this._options);
     }
 
     calculateVirtualScrollHeight(): number {
