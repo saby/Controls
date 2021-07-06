c31a71bb7b77d0d7a6d0e645ff47517ec67f7e36
ee.zolotova <ee.zolotova@tensor.ru>
2021-07-06 11:05:29 +0300

    https://online.sbis.ru/opendoc.html?guid=a00c4c13-b167-4fef-9e23-2b53515df35e

--- Controls/_filter/View.ts
+++ Controls/_filter/View.ts
@@ -239,7 +239,7 @@ class FilterView extends Control<IFilterViewOptions, IFilterReceivedState> imple
                 (!oldItem || !oldItemIsFrequent || optionsToCheck.reduce(getOptionsChecker(oldItem, newItem), false)
                     || (valueChanged && configs && !configs[newItem.name]) || needHistoryReload)
             ) {
-                if (valueChanged && !isEqual(newItem.value, newItem.value)) {
+                if (valueChanged && !isEqual(newItem.value, newItem.resetValue)) {
                     result.push(newItem);
                 } else if (configs && configs[newItem.name]) {
                     // Загрузим перед открытием
