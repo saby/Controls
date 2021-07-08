8dbb669afa877bf492208b2c3497f19a1784aba5
ma.mochalov <ma.mochalov@tensor.ru>
2021-07-08 13:46:13 +0300

    https://online.sbis.ru/opendoc.html?guid=876b264c-8410-4733-83ed-3b71232ca65a

--- Controls-demo/Decorator/Money/Precision/Index.ts
+++ Controls-demo/Decorator/Money/Precision/Index.ts
@@ -3,10 +3,7 @@ import * as template from 'wml!Controls-demo/Decorator/Money/Precision/Template'
 
 class PrecisionDemo extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
-
-    protected _value = '12345.67';
-
-    static _theme: string[] = ['Controls/Classes'];
+    protected _value: string = '12345.67';
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
