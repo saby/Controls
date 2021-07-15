33bd9a1d6fbd2dc115cf5c7c07ffb43d78ef2fc4
dv.onischuk <dv.onischuk@tensor.ru>
2021-07-15 10:30:55 +0300

    https://online.sbis.ru/opendoc.html?guid=8825f507-1e0c-4153-8c65-db0526ec9dd1

--- Controls-demo/Popup/Page/Loaders/DataLoader.ts
+++ Controls-demo/Popup/Page/Loaders/DataLoader.ts
@@ -1,8 +1,8 @@
-import {loadModule} from 'Controls/_popup/utils/moduleHelper';
+import {load} from 'Core/library';
 import {DataLoader} from 'Controls/dataSource';
 
 export function loadData(prefetchConfig: Record<string, any>): Promise<any> {
-    return loadModule(prefetchConfig.configLoader).then((loaderModule) => {
+    return load(prefetchConfig.configLoader).then((loaderModule) => {
         return new DataLoader().loadEvery(loaderModule.getConfig(prefetchConfig.configLoaderArguments));
     });
 }
