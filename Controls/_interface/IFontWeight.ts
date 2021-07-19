fd8eca0cf561ec154a8bf782ff826a43f16ba7a9
aa.bykanov <aa.bykanov@tensor.ru>
2021-07-19 16:45:14 +0300

    https://online.sbis.ru/opendoc.html?guid=bd1febf9-f3ec-4424-aa46-456bdbcd4ae9

--- Controls/_interface/IFontWeight.ts
+++ Controls/_interface/IFontWeight.ts
@@ -1,11 +1,18 @@
 /**
  * @typedef {String} TFontWeight
+ * @description Допустимые значения для опции {@link Controls/interface:IFontWeight#fontWeight fontWeight}.
  * @variant default
  * @variant bold
  */
 export type TFontWeight = 'default' | 'bold';
 
 export interface IFontWeightOptions {
+    /**
+     * @name Controls/_interface/IFontWeight#fontWeight
+     * @cfg {TFontWeight} Начертание шрифта.
+     * @default default
+     * @demo Controls-demo/Decorator/Money/FontWeight/Index
+     */
     fontWeight: TFontWeight;
 }
 
@@ -17,10 +24,3 @@ export interface IFontWeightOptions {
 export default interface IFontWeight {
     readonly '[Controls/_interface/IFontWeight]': boolean;
 }
-
-/**
- * @name Controls/_interface/IFontWeight#fontWeight
- * @cfg {TFontWeight} Начертание шрифта
- * @default default
- * @demo Controls-demo/Decorator/Money/FontWeight/Index
- */
