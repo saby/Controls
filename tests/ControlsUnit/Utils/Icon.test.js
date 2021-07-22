3bbe920e24537a151b48698ddf2df8c0b93694dc
se.mihaylov <se.mihaylov@tensor.ru>
2021-07-22 11:22:04 +0300

    https://online.sbis.ru/opendoc.html?guid=d1e4150f-9a40-4b67-8938-41f0db5c8aff

--- tests/ControlsUnit/Utils/Icon.test.js
+++ tests/ControlsUnit/Utils/Icon.test.js
@@ -2,13 +2,13 @@ define([
    'Controls/Utils/Icon'
 ], function(Util) {
    describe('Controls/Util/Icon', () => {
-      it('getIcon', () => {
+      describe('getIcon', () => {
          it('returns url with external use svg syntax', () => {
-            assert.equal(Util.getIcon('Controls/iconModule:icon-done'), '/Controls/iconModule.svg#icon-done');
+            assert.equal(Util.getIcon('Controls/iconModule:icon-done'), '/resources/Controls/iconModule.svg#icon-done');
          });
 
          it('returns original icon with incorrect syntax', () => {
-            assert.equal(Util.icon('icon-done'), 'icon-done');
+            assert.equal(Util.getIcon('icon-done'), 'icon-done');
          });
       });
    });
